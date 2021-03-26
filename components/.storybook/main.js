5d1948f59b0b477c57df11b7baaf75ad7ac17790
Agata Anna Jedryszek <agata.jedryszek@bouvet.no>
2021-03-26 15:14:46 +0100

    set up toolchain with react, ts, storybook and rollup

---
+++ components/.storybook/main.js
@@ -0,0 +1,19 @@
+module.exports = {
+  "stories": [
+    "../src/**/*.stories.tsx",
+  ],
+  "addons": [
+    "@storybook/addon-links",
+    "@storybook/addon-actions",
+    "@storybook/addon-essentials"
+  ],
+  typescript: {
+    check: false,
+    checkOptions: {},
+    reactDocgen: 'react-docgen-typescript',
+    reactDocgenTypescriptOptions: {
+      shouldExtractLiteralValuesFromEnum: true,
+      propFilter: (prop) => (prop.parent ? !/node_modules/.test(prop.parent.fileName) : true),
+    },
+  },
+}
