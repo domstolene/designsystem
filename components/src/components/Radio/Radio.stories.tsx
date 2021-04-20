bbcc152898e3e6410355641da0787d35dbe2bcd6
Erlend Lokken <erlend.lokken@gmail.com>
2021-04-20 14:36:40 +0200

    Trigger change in src folder

--- components/src/components/Radio/Radio.stories.tsx
+++ components/src/components/Radio/Radio.stories.tsx
@@ -17,7 +17,6 @@ export const Radio = () => {
         <RadioButton value={1}>Test 1</RadioButton>
         <RadioButton value={2}>Test 2</RadioButton>
         <RadioButton value={3}>Test 3</RadioButton>
-        <RadioButton value={4}>Test 4</RadioButton>
       </RadioGroup>
     </div>
   );
