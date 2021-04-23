2e783a02c02b0b07571c14233d306c532a6af3ce
Agata Anna Jedryszek <agata.jedryszek@bouvet.no>
2021-04-23 15:03:36 +0200

    added variants for button component and button stories

--- components/src/components/Button/Button.stories.tsx
+++ components/src/components/Button/Button.stories.tsx
@@ -1,16 +1,67 @@
 import {Button as DDSButton, ButtonProps} from './Button';
+import { storyHTMLTemplate } from '../../storybook/storyHTMLTemplate';
+
 
 export default {
   title: 'Designsystem/Button',
-  component: DDSButton
+  component: DDSButton,
+  argTypes: {
+    label: { control: { type: 'text' } },
+    loading: {control: {type: 'boolean'}},
+    className: {control: {type: 'text'}},
+    href: {control: {type: 'text'}},
+  },
 };
 
-export const Button = (args:ButtonProps) => {
+export const Primary = (args:ButtonProps) => {
   return (
-    <div className="component-container">
-      <h2> Button </h2>
-      <hr />
-      <DDSButton {...args} label={args.label || 'Tekst'} />
-    </div>
+    storyHTMLTemplate('Button primary', <DDSButton {...args} label={args.label || 'tekst'} />)
+  )
+}
+
+export const Secondary = (args:ButtonProps) => {
+  return (
+    storyHTMLTemplate('Button secondary', <DDSButton {...args} label={args.label || 'tekst'} purpose='secondary' />)
+  )
+}
+export const Danger = (args:ButtonProps) => {
+  return (
+    storyHTMLTemplate('Button danger', <DDSButton {...args} label={args.label || 'tekst'} purpose='danger' />)
+  )
+}
+
+export const Ghost = (args:ButtonProps) => {
+  return (
+    storyHTMLTemplate('Button ghost',
+     <>
+     <DDSButton {...args} label={args.label || 'tekst'} purpose='primary' form='ghost' />
+     <DDSButton {...args} label={args.label || 'tekst'} purpose='secondary' form='ghost' />
+     <DDSButton {...args} label={args.label || 'tekst'} purpose='danger' form='ghost' />
+     </>
+     )
+  )
+}
+
+export const Borderless = (args:ButtonProps) => {
+  return (
+    storyHTMLTemplate('Button borderless',
+     <>
+     <DDSButton {...args} label={args.label || 'tekst'} purpose='primary' form='borderless' />
+     <DDSButton {...args} label={args.label || 'tekst'} purpose='secondary' form='borderless' />
+     <DDSButton {...args} label={args.label || 'tekst'} purpose='danger' form='borderless' />
+     </>
+     )
+  )
+}
+
+export const Rounded = (args:ButtonProps) => {
+  return (
+    storyHTMLTemplate('Button rounded',
+     <>
+     <DDSButton {...args} label={args.label || 'tekst'} purpose='primary' form='rounded' />
+     <DDSButton {...args} label={args.label || 'tekst'} purpose='secondary' form='rounded' />
+     <DDSButton {...args} label={args.label || 'tekst'} purpose='danger' form='rounded' />
+     </>
+     )
   )
 }
--- components/src/components/Button/Button.tsx
+++ components/src/components/Button/Button.tsx
@@ -1,30 +1,119 @@
-import React, { ButtonHTMLAttributes, ElementType, forwardRef, InputHTMLAttributes } from 'react';
-import styled from 'styled-components';
+import React, { ButtonHTMLAttributes, ElementType, forwardRef } from 'react';
+import { buttonTokens as tokens } from './buttonTokens';
+import styled, { css } from 'styled-components';
+
+const buttonContentStyle = (purpose: Purpose, form: Form) => {
+    
+    return css`
+        color: ${tokens.colors[form][purpose].text};
+        border: ${tokens.borderWidth} solid;
+        border-color: ${tokens.colors[form][purpose].border};
+        background-color: ${tokens.colors[form][purpose].default};
+        box-shadow: ${tokens.boxShadow[form]};
+        border-radius: ${tokens.borderRadius[form]};
+        ${form === 'borderless' && css`
+            text-decoration: underline;
+            text-decoration-color: transparent;
+        `
+        }
+        ${form !== 'borderless' ? css`
+            &:hover {
+                background-color: ${tokens.colors[form][purpose].hover};
+                border-color: ${tokens.colors[form][purpose].hover_border};
+                ${form === 'ghost' && css`
+                    color: ${tokens.colors[form][purpose].hover_text};
+                    box-shadow: 0 0 0 1px ${tokens.colors[form][purpose].hover_border};
+                `
+                }
+            }
+            &:active {
+                background-color: ${tokens.colors[form][purpose].active};
+                border-color: ${tokens.colors[form][purpose].active_border};
+                ${form === 'ghost' && css`
+                    color: ${tokens.colors[form][purpose].active_text};
+                    box-shadow: 0 0 0 1px ${tokens.colors[form][purpose].active_border};
+                `
+                }
+            }
+        `
+        : css`
+            &:hover {
+                color: ${tokens.colors.borderless[purpose].hover};
+                text-decoration-color: ${tokens.colors.borderless[purpose].hover};
+            }
+            &:active {
+                color: ${tokens.colors.borderless[purpose].active};
+                text-decoration-color: ${tokens.colors.borderless[purpose].active};
+            }
+        `
+        }
+    `;
+}
+
+const ButtonContent = styled.span<{label?: string, size: Size, purpose: Purpose, form: Form}>`
+    display: flex;
+    align-items: center;
+    position: relative;
+    transition: background-color 0.2s, text-decoration-color 0.2s, box-shadow 0.2s;
+    ${({purpose, form}) => buttonContentStyle(purpose, form)}
+
+    ${({size, label}) => {
+        if(size) {
+            return (!label) ? css`
+                ${tokens.font.icon[size]}
+                padding: ${tokens.sizes.padding.icon[size]};
+            `
+            : css`
+                ${tokens.font.text[size]}
+                padding: ${tokens.sizes.padding.text[size]}px;
+            `
+        }
+    }
+    }
+    &:focus {
+        outline: none;
+    }
+`;
 
 const ButtonWrapper = styled.button`
+    display: inline-block;
+    border: none;
+    cursor: pointer;
+    box-shadow: none;
+    padding: 0;
+    background-color: transparent;
 
+    &:focus > ${ButtonContent} {
+        outline: ${tokens.focusOutline.width} solid ${tokens.focusOutline.color};
+        outline-offset: 2px;
+    }
+    &:focus {
+        outline: none;
+    }
 `;
 
-const ButtonContent = styled.span`
-
-`;
+type Size = 'small' | 'medium' | 'large';
+type Purpose = 'primary' | 'secondary' | 'danger';
+type Form = 'filled' | 'ghost' | 'rounded' | 'borderless';
+type IconPosition = 'left' | 'right';
 
 export type ButtonProps = {
-    size: 'small' | 'medium' | 'large';
+    size: Size;
     label?: string;
     disabled?: boolean;
-    purpose: 'primary' | 'secondary' | 'danger';
-    iconPosition?: 'left' | 'right';
-    form: 'filled' | 'ghost' | 'rounded' | 'borderless';
+    purpose: Purpose;
+    iconPosition?: IconPosition;
+    form: Form;
     href?: string;
     loading?: boolean;
     className?: string;
     style?: React.CSSProperties;
+    icon?: JSX.Element;
 } & ButtonHTMLAttributes<HTMLButtonElement>;
 
 
 export const Button = forwardRef<HTMLButtonElement, ButtonProps>(
-    ({label, disabled, purpose = 'primary', size = 'medium', iconPosition, form = 'filled', href, loading, className, style, ...rest}, ref) => {
+    ({label, disabled, purpose, size, iconPosition, form, href, loading, className, style, ...rest}, ref) => {
 
         const as: ElementType = href ? 'a' : 'button';
 
@@ -45,13 +134,20 @@ export const Button = forwardRef<HTMLButtonElement, ButtonProps>(
             size,
             tabIndex: -1,
             className,
-            style,
-            ...rest
+            style
         }
         return (
             <ButtonWrapper {...wrapperProps}>
-                <ButtonContent {...contentProps}>{label}</ButtonContent>
+                <ButtonContent {...contentProps}>
+                    {label}
+                </ButtonContent>
             </ButtonWrapper>
         );
     }
 )
+
+Button.defaultProps = {
+    size: 'medium',
+    purpose: 'primary',
+    form: 'filled'
+}
