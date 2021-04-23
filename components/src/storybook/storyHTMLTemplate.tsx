602f4b8c604b54c386bf6fe780796b6f4cae6a8d
Agata Anna Jedryszek <agata.jedryszek@bouvet.no>
2021-04-23 15:00:00 +0200

    added simple template to storybook

---
+++ components/src/storybook/storyHTMLTemplate.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export const storyHTMLTemplate = (title: string, children: React.ReactNode) => {
+    return (
+    <div className="component-container">
+      <h2> {title} </h2>
+      <hr />
+      <div className='flex-components'>
+        {children}
+      </div>
+    </div>
+    );
+}
