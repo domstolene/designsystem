c16bbe8001f33caba927e70d408c80f040e36599
Agata Anna Jedryszek <agata.jedryszek@bouvet.no>
2021-04-26 11:46:50 +0200

    sorted stories alphabetically in storybook and placed introduction as first

--- components/.storybook/preview.js
+++ components/.storybook/preview.js
@@ -1,4 +1,12 @@
 
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
+  options: {
+    storySort: (a, b) => {
+      console.log(a,b);
+      if(a[1].id === 'design-system-introduction--page') return -1;
+      if(b[1].id === 'design-system-introduction--page') return 1;
+      return a[1].kind === b[1].kind ? 0 : a[1].id.localeCompare(b[1].id, undefined, { numeric: true });
+    }
+  },
 }
