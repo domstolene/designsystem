d8c37f7d4c22d51a81f5e83b2dcc791d8857a02a
Agata Anna Jedryszek <agata.jedryszek@bouvet.no>
2021-05-10 13:19:06 +0200

    added firstLetterToUpperCase util

--- components/src/helpers/functions.tsx
+++ components/src/helpers/functions.tsx
@@ -1,3 +1,5 @@
 export const calculateHeightWithLineHeight = (lineHeight: number, fontSize: number) => {
     return lineHeight * 0.01 * fontSize;
 }
+
+export const firstLetterToUpperCase = (text: string) => text.charAt(0).toUpperCase() + text.slice(1);
