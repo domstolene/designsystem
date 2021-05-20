a6c77429551688f858b096dad7a81c3114a0bb1c
Agata Jedryszek <agata.jedryszek@bouvet.no>
2021-05-20 09:47:20 +0200

    TextInput stories cleanup

--- components/src/components/TextInput/TextInput.stories.tsx
+++ components/src/components/TextInput/TextInput.stories.tsx
@@ -1,4 +1,5 @@
 import { storyHTMLTemplate } from '../../storybook/storyHTMLTemplate';
+import { storyHTMLGridTemplate } from '../../storybook/storyHTMLGridTemplate';
 import TextInput, { TextInputProps } from './TextInput';
 
 export default {
@@ -13,92 +14,70 @@ export default {
   },
 };
 
-export const withLabel = (args: TextInputProps) => {
+export const TextInputOverview = (args: TextInputProps) => {
     return (
-        storyHTMLTemplate('TextInput', <TextInput {...args} label={args.label || 'Label'} id='test' />)
-    );
-}
-
-export const noLabel = (args: TextInputProps) => {
-    return (
-        storyHTMLTemplate('TextInput no label', <TextInput {...args} id='test' />)
-    );
-}
-
-export const Multiline = (args: TextInputProps) => {
-    return (
-        storyHTMLTemplate('TextInput multiline (Textarea)', <TextInput {...args} multiline label={args.label || 'Label'} id='test' />)
-    );
-}
-
-export const Disabled = (args: TextInputProps) => {
-    return (
-        storyHTMLTemplate('TextInput disabled', <TextInput {...args} label={args.label || 'Label'} id='test' disabled value='Disabled inputfelt' />)
-    );
-}
-
-export const Readonly = (args: TextInputProps) => {
-    return (
-        storyHTMLTemplate('TextInput readonly', <TextInput {...args} label={args.label || 'Label'} id='test' readOnly value='Readonly inputfelt' />)
-    );
-}
-
-export const Error = (args: TextInputProps) => {
-    return (
-        storyHTMLTemplate('TextInput error', <TextInput {...args} label={args.label || 'Label'} id='test' errorMessage={args.errorMessage || 'Dette er en feilmelding ved valideringsfeil'} />)
-    );
-}
-
-export const WithTip = (args: TextInputProps) => {
-    return (
-        storyHTMLTemplate('TextInput with tip', <TextInput {...args} label={args.label || 'Label'} id='test' tip={args.tip || 'Dette er en hjelpetekst'} />)
-    );
-}
-
-export const WithCharCounter = (args: TextInputProps) => {
-    return (
-        storyHTMLTemplate('TextInput with Character Counter', <TextInput {...args} label={args.label || 'Label'} id='test' tip={args.tip || 'Dette er en hjelpetekst'} maxCharCount={20} />)
-    );
-}
-
-export const TextInputVariations = (args: TextInputProps) => {
-    return (
-        storyHTMLTemplate('TextInput variations',
-        <>
-        <TextInput {...args} label={args.label || 'Label'} id='test' />
-        <TextInput {...args} label={args.label || 'Label'} id='test' disabled value='Disabled inputfelt' />
-        <TextInput {...args} label={args.label || 'Label'} id='test' readOnly value='Readonly inputfelt' />
-        <TextInput {...args} label={args.label || 'Label'} id='test3' errorMessage={args.errorMessage || 'Dette er en feilmelding ved valideringsfeil'} />
-        <TextInput {...args} label={args.label || 'Label'} id='test5' tip={args.tip || 'Dette er en hjelpetekst'} />
-        <TextInput {...args} label={args.label || 'Label'} id='test7' tip={args.tip || 'Dette er en hjelpetekst'} maxCharCount={20} />
-        <TextInput {...args} id='test2' />
-        <TextInput {...args} id='test' disabled value='Disabled inputfelt' />
-        <TextInput {...args} id='test' readOnly value='Readonly inputfelt' />
-        <TextInput {...args} id='test4' errorMessage={args.errorMessage || 'Dette er en feilmelding ved valideringsfeil'} />
-        <TextInput {...args} id='test6' tip={args.tip || 'Dette er en hjelpetekst'} />
-        <TextInput {...args} id='test8' tip={args.tip || 'Dette er en hjelpetekst'} maxCharCount={20} />
-        </>
+        storyHTMLTemplate('TextInput - overview',
+            <>
+                <TextInput {...args} label={args.label || 'Label'} id='test' />
+                <TextInput {...args} id='test2' />
+                <TextInput {...args} label={args.label || 'Label'} id='test' disabled value='Disabled inputfelt' />
+                <TextInput {...args} id='test' disabled value='Disabled inputfelt' />
+                <TextInput {...args} label={args.label || 'Label'} id='test' readOnly value='Readonly inputfelt' />
+                <TextInput {...args} id='test' readOnly value='Readonly inputfelt' />
+                <TextInput {...args} label={args.label || 'Label'} id='test3' errorMessage={args.errorMessage || 'Dette er en feilmelding ved valideringsfeil'} />
+                <TextInput {...args} id='test4' errorMessage={args.errorMessage || 'Dette er en feilmelding ved valideringsfeil'} />
+                <TextInput {...args} label={args.label || 'Label'} id='test5' tip={args.tip || 'Dette er en hjelpetekst'} />
+                <TextInput {...args} id='test6' tip={args.tip || 'Dette er en hjelpetekst'} />
+                <TextInput {...args} label={args.label || 'Label'} id='test7' tip={args.tip || 'Dette er en hjelpetekst med character count'} maxCharCount={20} />
+                <TextInput {...args} id='test8' tip={args.tip || 'Dette er en hjelpetekst med character count'} maxCharCount={20} />
+            </>
         )
     );
 }
 
-export const TextareaVariations = (args: TextInputProps) => {
+export const TextareaOverview = (args: TextInputProps) => {
     return (
-        storyHTMLTemplate('Textarea variations',
-        <>
-        <TextInput {...args} multiline label={args.label || 'Label'} id='test' />
-        <TextInput {...args} multiline label={args.label || 'Label'} id='test' disabled value='Disabled textarea' />
-        <TextInput {...args} multiline label={args.label || 'Label'} id='test' readOnly value='Readonly textarea' />
-        <TextInput {...args} multiline label={args.label || 'Label'} id='test3' errorMessage={args.errorMessage || 'Dette er en feilmelding ved valideringsfeil'} />
-        <TextInput {...args} multiline label={args.label || 'Label'} id='test5' tip={args.tip || 'Dette er en hjelpetekst'} />
-        <TextInput {...args} multiline label={args.label || 'Label'} id='test7' tip={args.tip || 'Dette er en hjelpetekst'} maxCharCount={20} />
-        <TextInput {...args} multiline id='test2' />
-        <TextInput {...args} multiline id='test' disabled value='Disabled textarea' />
-        <TextInput {...args} multiline id='test' readOnly value='Readonly textarea' />
-        <TextInput {...args} multiline id='test4' errorMessage={args.errorMessage || 'Dette er en feilmelding ved valideringsfeil'} />
-        <TextInput {...args} multiline id='test6' tip={args.tip || 'Dette er en hjelpetekst'} />
-        <TextInput {...args} multiline id='test8' tip={args.tip || 'Dette er en hjelpetekst'} maxCharCount={20} />
-        </>
+        storyHTMLGridTemplate('Textarea (multiline) - overview',
+            <>
+                <TextInput {...args} multiline label={args.label || 'Label'} id='test' />
+                <TextInput {...args} multiline id='test2' />
+                <TextInput {...args} multiline label={args.label || 'Label'} id='test' disabled value='Disabled textarea' />
+                <TextInput {...args} multiline id='test' disabled value='Disabled textarea' />
+                <TextInput {...args} multiline label={args.label || 'Label'} id='test' readOnly value='Readonly textarea' />
+                <TextInput {...args} multiline id='test' readOnly value='Readonly textarea' />
+                <TextInput {...args} multiline label={args.label || 'Label'} id='test3' errorMessage={args.errorMessage || 'Dette er en feilmelding ved valideringsfeil'} />
+                <TextInput {...args} multiline id='test4' errorMessage={args.errorMessage || 'Dette er en feilmelding ved valideringsfeil'} />
+                <TextInput {...args} multiline label={args.label || 'Label'} id='test5' tip={args.tip || 'Dette er en hjelpetekst'} />
+                <TextInput {...args} multiline id='test6' tip={args.tip || 'Dette er en hjelpetekst'} />
+                <TextInput {...args} multiline label={args.label || 'Label'} id='test7' tip={args.tip || 'Dette er en hjelpetekst med character count'} maxCharCount={20} />
+                <TextInput {...args} multiline id='test8' tip={args.tip || 'Dette er en hjelpetekst med character count'} maxCharCount={20} />
+            </>,
+            '20px',
+            2
         )
+    );
+}
+
+export const Default = (args: TextInputProps) => {
+    return (
+        storyHTMLTemplate('TextInput - default', <TextInput {...args} id='test' />)
+        );
+    }
+
+export const InputWithLabel = (args: TextInputProps) => {
+    return (
+        storyHTMLTemplate('TextInput - with label', <TextInput {...args} label={args.label || 'Label'} id='test' />)
+    );
+}
+
+export const Textarea = (args: TextInputProps) => {
+    return (
+        storyHTMLTemplate('Textarea (multiline) - default', <TextInput {...args} id='test' multiline />)
+    );
+}
+
+export const TextareaWithLabel = (args: TextInputProps) => {
+    return (
+        storyHTMLTemplate('Textarea (multiline) - default', <TextInput {...args} label={args.label || 'Label'} id='test' multiline />)
     );
 }
