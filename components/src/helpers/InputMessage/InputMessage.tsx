f2deca063a6da171a04b13beded2ba87c440d7a1
Agata Jedryszek <agata.jedryszek@bouvet.no>
2021-05-21 10:52:27 +0200

    InputMessage cleanup, fixed maxWidth property

--- components/src/helpers/InputMessage/InputMessage.tsx
+++ components/src/helpers/InputMessage/InputMessage.tsx
@@ -2,24 +2,23 @@ import styled, { css } from 'styled-components';
 import ReportProblemOutlinedIcon from '@material-ui/icons/ReportProblemOutlined';
 import InlineIconWrapper from '../InlineIconWrapper';
 import { inputMessageTokens as tokens} from './inputMessageTokens';
+import * as CSS from 'csstype';
 
-const InputMessageWrapper = styled.div<{messageType: MessageType, maxWidth?: string}>`
+const InputMessageWrapper = styled.div<{messageType: MessageType, maxWidth: CSS.Property.MaxWidth<string>}>`
     display: flex;
+    align-items: center;
     width: fit-content;
     word-break: break-word;
-    max-width: ${({ maxWidth }) => maxWidth ? maxWidth : '100%'};
-    padding: ${tokens.padding};
+    ${tokens.base}
     ${({messageType}) => css`
-        ${tokens.font[messageType]}
-        color: ${tokens.colors[messageType].text};
-        background-color: ${tokens.colors[messageType].background};
+        ${tokens[messageType].base}
 
     `}
+    max-width: ${({ maxWidth }) => maxWidth};
 
     svg {
-        margin-right: ${tokens.margin.icon.marginRight};
+        margin-right: ${tokens.icon.spaceRight};
         position: relative;
-        top: 0.25em;
     }
 `;
 
@@ -28,7 +27,7 @@ type MessageType = 'error' | 'tip';
 type Props = {
     message: string,
     messageType: MessageType;
-    maxWidth?: string;
+    maxWidth: CSS.Property.MaxWidth<string>;
 }
 
 function InputMessage({message, messageType, maxWidth}: Props) {
@@ -42,6 +41,10 @@ function InputMessage({message, messageType, maxWidth}: Props) {
             </div>
         </InputMessageWrapper>
     )
+}
+
+InputMessage.defaultProps = {
+    maxWidth: tokens.defaultMaxWidth
 }
 
 export default InputMessage;
--- components/src/helpers/InputMessage/inputMessageTokens.tsx
+++ components/src/helpers/InputMessage/inputMessageTokens.tsx
@@ -1,4 +1,6 @@
 import { ddsBaseTokens } from '@norges-domstoler/dds-design-tokens';
+import { CSSObject } from 'styled-components';
+import * as CSS from 'csstype';
 
 const {
     colors: Colors,
@@ -6,25 +8,36 @@ const {
     fontPackages: FontPackages,
 } = ddsBaseTokens;
 
-export const inputMessageTokens = {
+const base: CSSObject = {
     padding: `${Spacing.SizesDdsSpacingLocalX025} ${Spacing.SizesDdsSpacingLocalX05}`,
-    font: {
-        tip: FontPackages.sansRegularx0_875.base,
-        error: FontPackages.sansBoldx0_875.base
+}
+
+const defaultMaxWidth: CSS.Property.MaxWidth<string> = '100%';
+
+const tipBase: CSSObject = {
+    ...FontPackages.sansRegularx0_875.base,
+    color: Colors.DdsColorNeutralsGray6,
+    backgroundColor: Colors.DdsColorNeutralsWhite
+}
+
+const errorBase: CSSObject = {
+    ...FontPackages.sansBoldx0_875.base,
+    color: Colors.DdsColorDangerBase,
+    backgroundColor: Colors.DdsColorDangerLightest
+}
+
+export const inputMessageTokens = {
+
+    padding: `${Spacing.SizesDdsSpacingLocalX025} ${Spacing.SizesDdsSpacingLocalX05}`,
+    base: base,
+    defaultMaxWidth: defaultMaxWidth,
+    tip: {
+        base: tipBase
     },
-    colors: {
-        tip: {
-            text: Colors.DdsColorNeutralsGray6,
-            background: Colors.DdsColorNeutralsWhite
-        },
-        error: {
-            text: Colors.DdsColorDangerBase,
-            background: Colors.DdsColorDangerLightest
-        }
+    error: {
+        base: errorBase
     },
-    margin: {
-        icon: {
-            marginRight: Spacing.SizesDdsSpacingLocalX05
-        }
-    }
+    icon: {
+        spaceRight: Spacing.SizesDdsSpacingLocalX05
+    },
 }
