23c717678bdde12f5f859b7e5894641bb47a4f79
Agata Jedryszek <agata.jedryszek@bouvet.no>
2021-05-31 12:29:09 +0200

    updated reference tokens with TextInput, inputLabel, focus etc

--- tokens/src/ddsReferenceTokens.ts
+++ tokens/src/ddsReferenceTokens.ts
@@ -1,7 +1,51 @@
 import { ddsBaseTokens } from "./ddsBaseTokens";
 
+const {
+  colors: Colors,
+  spacing: Spacing,
+  fontPackages: FontPackages,
+  borderRadius: BorderRadius,
+  border: Border,
+} = ddsBaseTokens;
+
+const textDefault = {
+  textColor: Colors.DdsColorNeutralsGray9,
+  font: FontPackages.sansRegularx1.base,
+};
+
+const focus = {
+  colorDefault: Colors.DdsColorWarningDarker,
+  outlineWidth: Border.BordersDdsBorderStyle1StrokeWeight,
+  color__TextInput: Colors.DdsColorPrimaryBase,
+};
+
+const inputLabel = {
+  textColor: Colors.DdsColorNeutralsGray7,
+  font: FontPackages.sansBoldx0_875,
+};
+
 export const ddsReferenceTokens = {
-  color: {
-    focus_default: ddsBaseTokens.colors.DdsColorWarningDarker,
+  focus: focus,
+  inputLabel: inputLabel,
+  textDefault: textDefault,
+  textInput: {
+    input: {
+      borderRadius: BorderRadius.RadiiDdsBorderRadius1Radius,
+      borderColor: Colors.DdsColorPrimaryBase,
+      textColor: textDefault.textColor,
+      font: FontPackages.sansRegularx1_125.base,
+      hover: {
+        backgroundColor: Colors.DdsColorPrimaryLightest,
+      },
+      focus: {
+        borderColor: focus.color__TextInput,
+      },
+    },
+    label: {
+      base: inputLabel,
+      focus: {
+        textColor: focus.color__TextInput,
+      },
+    },
   },
 };
