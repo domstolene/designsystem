5f8b7cd9af7921076afa19b0ea965f93a0e496db
Agata Jedryszek <agata.jedryszek@bouvet.no>
2021-07-29 12:44:48 +0200

    added Breadcrumbs test

--- components/src/components/Breadcrumbs/Breadcrumbs.spec.tsx
+++ components/src/components/Breadcrumbs/Breadcrumbs.spec.tsx
@@ -1,7 +1,24 @@
 import '@testing-library/jest-dom';
+import { render } from '@testing-library/react';
+import {Breadcrumbs, Breadcrumb } from '.';
 
-describe('<Breadcrumbs />', () => {
-    it('dummy test', () => {
-        expect(true).toBeTruthy();
+describe('Breadcrumbs', () => {
+    it('has aria-label', () => {
+        render (
+            <Breadcrumbs id='test'>
+                <Breadcrumb>
+                    Side 1
+                </Breadcrumb>
+                    Side 2
+                <Breadcrumb>
+                </Breadcrumb>
+                <Breadcrumb>
+                    Side 3
+                </Breadcrumb>
+            </Breadcrumbs>
+        )
     });
+
+    const breadcrumbs = document.getElementById('test');
+    expect(breadcrumbs).toHaveAttribute('aria-label', 'breadcrumbs');
 });
