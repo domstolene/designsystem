cadbac35f0d1facc5c5a0fc620b331f4d54dfc95
Agata Jedryszek <agata.jedryszek@bouvet.no>
2021-07-29 13:15:50 +0200

    added Button tests

--- components/src/components/Button/Button.spec.tsx
+++ components/src/components/Button/Button.spec.tsx
@@ -1,7 +1,19 @@
 import '@testing-library/jest-dom';
+import { render, screen } from '@testing-library/react';
+import { Button } from '.';
 
 describe('<Button />', () => {
   it('dummy test', () => {
     expect(true).toBeTruthy();
   });
+  it('should have a label', () => {
+    const label = 'button label';
+    render(<Button label={label} />);
+    expect(screen.queryByText(label)).toBeDefined;
+  });
+  it('renders an anchor element if href prop is provided', () => {
+    render(<Button href='/' />)
+    const button = screen.getByRole('link');
+    expect(button).toBeInTheDocument();
+  })
 });
