f2aaf39418d14b53ca9949a7b31f7a72f8f52c67
Agata Jedryszek <agata.jedryszek@bouvet.no>
2021-08-11 14:37:26 +0200

    added several token filters and fixed font formatting for CSS in build

--- tokens/dds/build.js
+++ tokens/dds/build.js
@@ -1,23 +1,23 @@
-const StyleDictionary = require('style-dictionary');
+const StyleDictionary = require("style-dictionary");
 
-console.log('Build started...');
-console.log('\n==============================================');
+console.log("Build started...");
+console.log("\n==============================================");
 
 function fileHeader(options, commentStyle) {
-  var to_ret = '';
+  var to_ret = "";
   // for backward compatibility we need to have the user explicitly hide them
-  var showFileHeader = (options) ? options.showFileHeader : true;
+  var showFileHeader = options ? options.showFileHeader : true;
   if (showFileHeader) {
-    if (commentStyle === 'short') {
-      to_ret += '\n';
-      to_ret += '// Do not edit directly\n';
-      to_ret += '// Generated on ' + new Date().toUTCString() + '\n';
-      to_ret += '\n';
+    if (commentStyle === "short") {
+      to_ret += "\n";
+      to_ret += "// Do not edit directly\n";
+      to_ret += "// Generated on " + new Date().toUTCString() + "\n";
+      to_ret += "\n";
     } else {
-      to_ret += '/**\n';
-      to_ret += ' * Do not edit directly\n';
-      to_ret += ' * Generated on ' + new Date().toUTCString() + '\n';
-      to_ret += ' */\n\n';
+      to_ret += "/**\n";
+      to_ret += " * Do not edit directly\n";
+      to_ret += " * Generated on " + new Date().toUTCString() + "\n";
+      to_ret += " */\n\n";
     }
   }
 
@@ -27,37 +27,41 @@ function fileHeader(options, commentStyle) {
 // CUSTOM TRANFORMS
 
 StyleDictionary.registerTransform({
-  name: 'pixel/px',
-  type: 'value',
-  matcher: function(prop) {
-      // here we are using a custom attribute, declared in the property, to match the values where apply the transform
-      return prop.unit === 'pixel' || prop.unit === 'pixels';
+  name: "pixel/px",
+  type: "value",
+  matcher: function (prop) {
+    // here we are using a custom attribute, declared in the property, to match the values where apply the transform
+    return prop.unit === "pixel" || prop.unit === "pixels";
   },
-  transformer: function(prop) {
-      return `${prop.value}px`;
-  }
+  transformer: function (prop) {
+    return `${prop.value}px`;
+  },
 });
 
 StyleDictionary.registerTransform({
-    name: '%/em',
-    type: 'value',
-    matcher: function(prop) {
-        return prop.unit === 'percent' && prop.attributes.category === '$dds-font';
-    },
-    transformer: function(prop) {
-        return `${prop.value/100}em`;
-    }
-  });
+  name: "%/em",
+  type: "value",
+  matcher: function (prop) {
+    return prop.unit === "percent" && prop.attributes.category === "$dds-font";
+  },
+  transformer: function (prop) {
+    return `${prop.value / 100}em`;
+  },
+});
 
 StyleDictionary.registerTransform({
-    name: 'fontFamily/string',
-    type: 'value',
-    matcher: function(prop) {
-        return prop.type === 'string' && prop.fontValue === 'fontFamily';
-    },
-    transformer: function(prop) {
-        return `'${prop.value}'`;
-    }
+  name: "fontFamily/string",
+  type: "value",
+  matcher: function (prop) {
+    return (
+      prop.type === "string" &&
+      prop.attributes.category === "$dds-font" &&
+      prop.path[prop.path.length - 1] === "fontFamily"
+    );
+  },
+  transformer: function (prop) {
+    return `'${prop.value}'`;
+  },
 });
 
 //TRANSFORM EXAMPLES
@@ -87,27 +91,33 @@ StyleDictionary.registerTransform({
 //   }
 // });
 
-
-
 // REGISTER THE CUSTOM TRANFORM GROUPS
 
 // if you want to see what a pre-defined group contains, uncomment the next line:
 // console.log(StyleDictionary.transformGroup['group_name']);
 
 StyleDictionary.registerTransformGroup({
-  name: 'custom/js',
-  transforms: StyleDictionary.transformGroup['js'].concat(['%/em'])
+  name: "custom/js",
+  transforms: StyleDictionary.transformGroup["js"].concat(["%/em"]),
 });
 
 StyleDictionary.registerTransformGroup({
-  name: 'custom/css',
-  transforms: StyleDictionary.transformGroup['css'].concat(['pixel/px', '%/em', 'fontFamily/string'])
+  name: "custom/css",
+  transforms: StyleDictionary.transformGroup["css"].concat([
+    "pixel/px",
+    "%/em",
+    "fontFamily/string",
+  ]),
 });
 
 StyleDictionary.registerTransformGroup({
-    name: 'custom/scss',
-    transforms: StyleDictionary.transformGroup['scss'].concat(['pixel/px', '%/em', 'fontFamily/string'])
-  });
+  name: "custom/scss",
+  transforms: StyleDictionary.transformGroup["scss"].concat([
+    "pixel/px",
+    "%/em",
+    "fontFamily/string",
+  ]),
+});
 
 // REGISTER A CUSTOM FORMAT (example)
 
@@ -121,37 +131,99 @@ StyleDictionary.registerTransformGroup({
 // });
 
 StyleDictionary.registerFormat({
-  name: 'custom/javascript/es6',
-  formatter: function(dictionary) {
-    return fileHeader(this.options) + dictionary.allProperties.map(token => {
-        let output = `export const ${token.name} = ${JSON.stringify(token.value)};`;
-        if(token.unit === 'pixel' || token.unit === 'pixels') {
-          output = `export const ${token.name} = "${token.value}px";\nexport const ${token.name}NumberPx = ${token.value};`;
-        }
-        if(token.comment) {
-          output += ` // ${token.comment}`;
-        }
-        return output;
-      }).join(`\n`);
-  }
-})
+  name: "custom/javascript/es6",
+  formatter: function (dictionary) {
+    return (
+      fileHeader(this.options) +
+      dictionary.allProperties
+        .map((token) => {
+          let output = `export const ${token.name} = ${JSON.stringify(
+            token.value
+          )};`;
+          if (token.unit === "pixel" || token.unit === "pixels") {
+            output = `export const ${token.name} = "${token.value}px";\nexport const ${token.name}NumberPx = ${token.value};`;
+          }
+          if (token.comment) {
+            output += ` // ${token.comment}`;
+          }
+          return output;
+        })
+        .join(`\n`)
+    );
+  },
+});
 
 StyleDictionary.registerFormat({
-  name: 'javascript/objectExported',
-  formatter: function(dictionary) {
-    return `${fileHeader(this.options)} export const ${(this.name || 'ddsStyles')} = ${JSON.stringify(dictionary.properties, null, 2)};`;
-  }
-})
+  name: "javascript/objectExported",
+  formatter: function (dictionary) {
+    return `${fileHeader(this.options)} export const ${
+      this.name || "ddsStyles"
+    } = ${JSON.stringify(dictionary.properties, null, 2)};`;
+  },
+});
+
+// REGISTER A CUSTOM FILTER
+
+const isBorderToken = (token) =>
+  token.attributes.category === "borders" &&
+  token.path[token.path.length - 1] !== "strokeCap" &&
+  token.path[token.path.length - 1] !== "strokeJoin";
+
+const isRadiusToken = (token) =>
+  token.attributes.category === "radii" &&
+  token.path[token.path.length - 1] !== "smoothing" &&
+  token.path[token.path.length - 1] !== "radiusType";
+
+const isFontToken = (token) =>
+  token.attributes.category === "$dds-font" &&
+  token.path[token.path.length - 1] !== "_fontStyleOld";
+
+StyleDictionary.registerFilter({
+  name: "bordersFilter",
+  matcher: function (token) {
+    return token.value !== "" && isBorderToken(token);
+  },
+});
+
+StyleDictionary.registerFilter({
+  name: "radiiFilter",
+  matcher: function (token) {
+    return token.value !== "" && isRadiusToken(token);
+  },
+});
+
+StyleDictionary.registerFilter({
+  name: "fontFilter",
+  matcher: function (token) {
+    return token.value !== "" && isFontToken(token);
+  },
+});
+
+StyleDictionary.registerFilter({
+  name: "tokenFilter",
+  matcher: function (token) {
+    return (
+      token.value !== "" &&
+      (isFontToken(token) ||
+        isRadiusToken(token) ||
+        isBorderToken(token) ||
+        token.attributes.category === "$dds-color" ||
+        token.attributes.category === "$dds-grid" ||
+        token.attributes.category === "$dds-iconsize" ||
+        token.attributes.category === "sizes" ||
+        token.attributes.category === "$dds-innershadow" ||
+        token.attributes.category === "$dds-shadow")
+    );
+  },
+});
 
 // APPLY THE CONFIGURATION
 // IMPORTANT: the registration of custom transforms
 // needs to be done _before_ applying the configuration
-StyleDictionaryExtended = StyleDictionary.extend(__dirname + '/config.json');
-
+StyleDictionaryExtended = StyleDictionary.extend(__dirname + "/config.json");
 
 // FINALLY, BUILD ALL THE PLATFORMS
 StyleDictionaryExtended.buildAllPlatforms();
 
-
-console.log('\n==============================================');
-console.log('\nBuild completed!');
+console.log("\n==============================================");
+console.log("\nBuild completed!");
