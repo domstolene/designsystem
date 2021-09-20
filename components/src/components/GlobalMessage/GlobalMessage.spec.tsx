3814ef562d1c12dc2884d5b220b4423cebd503b1
Agata Jedryszek <agata.jedryszek@bouvet.no>
2021-09-20 15:33:55 +0200

    added GlobalMessage test

--- components/src/components/GlobalMessage/GlobalMessage.spec.tsx
+++ components/src/components/GlobalMessage/GlobalMessage.spec.tsx
@@ -1,7 +1,12 @@
 import '@testing-library/jest-dom';
+import { GlobalMessage } from './GlobalMessage';
+import { render, screen } from '@testing-library/react';
 
 describe('<GlobalMessage />', () => {
-  it('dummy test', () => {
-    expect(true).toBeTruthy();
+  it('has message', () => {
+    const message = 'This is a message';
+    render(<GlobalMessage message={message} />);
+    const messageElement = screen.getByText(message);
+    expect(messageElement).toBeDefined();
   });
 });
