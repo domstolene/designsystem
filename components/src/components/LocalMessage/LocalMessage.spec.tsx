f8ed505242251fed532fc1bbb95d396add9af237
Agata Jedryszek <agata.jedryszek@bouvet.no>
2021-09-20 15:34:19 +0200

    added LocalMessage test

--- components/src/components/LocalMessage/LocalMessage.spec.tsx
+++ components/src/components/LocalMessage/LocalMessage.spec.tsx
@@ -1,7 +1,12 @@
 import '@testing-library/jest-dom';
+import { LocalMessage } from './LocalMessage';
+import { render, screen } from '@testing-library/react';
 
 describe('<LocalMessage />', () => {
-  it('dummy test', () => {
-    expect(true).toBeTruthy();
+  it('has message', () => {
+    const message = 'This is a message';
+    render(<LocalMessage message={message} />);
+    const messageElement = screen.getByText(message);
+    expect(messageElement).toBeDefined();
   });
 });
