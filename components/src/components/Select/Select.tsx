c55c8dde1ec8418b7fe497cc7b02d22553681101
Erlend Lokken <erlend.lokken@gmail.com>
2021-10-19 09:31:59 +0200

    Diverse opprydding i Select-komponenten (#4)
    * Flytter Select-styles til egen fil
    
    * Eksporter egen SelectOption type
    
    * Fjerner sjekk på value fra isClearable

--- components/src/components/Select/Select.tsx
+++ components/src/components/Select/Select.tsx
@@ -1,137 +1,24 @@
 import CheckOutlinedIcon from '@material-ui/icons/CheckOutlined';
-import React, { forwardRef, HTMLAttributes, useState } from 'react';
+import * as CSS from 'csstype';
+import React, { forwardRef, useState } from 'react';
 import {
   components,
   default as ReactSelect,
   OptionProps,
-  Props as ReactSelectProps,
-  Styles
+  Props as ReactSelectProps
 } from 'react-select';
 import { NoticeProps } from 'react-select/src/components/Menu';
-import { Option } from 'react-select/src/filters';
-import styled, { css, CSSObject } from 'styled-components';
 import InputMessage from '../../helpers/InputMessage/InputMessage';
 import RequiredMarker from '../../helpers/RequiredMarker';
+import {
+  Container,
+  CustomStyles,
+  Label,
+  prefix,
+  SelectedIconWrapper,
+  Wrapper
+} from './Select.styles';
 import { selectTokens as tokens } from './Select.tokens';
-import { IconWrapper } from '../../helpers/IconWrapper';
-import scrollbarStyling from '../../helpers/scrollbarStyling';
-import { Typography } from '../Typography';
-import { typographyTokens } from '../Typography/Typography.tokens';
-import * as CSS from 'csstype';
-
-const prefix = 'dds-select';
-
-const Label = styled(Typography)`
-  display: block;
-  ${tokens.label.base}
-`;
-
-type StyledContainerProps = Pick<
-  SelectProps,
-  'errorMessage' | 'isDisabled' | 'readOnly' | 'label'
->;
-
-const Container = styled.div<StyledContainerProps>`
-  transition: 0.2s;
-  position: relative;
-  box-sizing: border-box;
-  ${tokens.container.base}
-  ${scrollbarStyling}
-  *::selection {
-    ${typographyTokens.selection.base}
-  }
-  ${({ label }) =>
-    label
-      ? css`
-          ${tokens.container.withLabel.base}
-        `
-      : css`
-          ${tokens.container.noLabel.base}
-          .${prefix}__control {
-            ${tokens.input.noLabel.base}
-          }
-        `}
-  &:hover {
-    ${tokens.container.hover.base}
-    box-shadow: 0 0 0 1px ${tokens.container.hover.base.borderColor};
-    ${Label} {
-      ${tokens.label.hover.base}
-    }
-  }
-  &:focus-within {
-    ${tokens.container.focus.base}
-    box-shadow: 0 0 0 1px ${tokens.container.focus.base.borderColor};
-    ${Label} {
-      ${tokens.label.focus.base}
-    }
-  }
-  ${({ errorMessage }) =>
-    errorMessage &&
-    css`
-      ${tokens.container.danger.base}
-      box-shadow: 0 0 0 1px ${tokens.container.danger.base.borderColor};
-      &:hover {
-        ${tokens.container.danger.hover.base}
-        box-shadow: 0 0 0 1px ${tokens.container.danger.hover.base.borderColor};
-      }
-      &:focus-within {
-        ${tokens.container.danger.focus.base}
-        box-shadow: 0 0 0 1px ${tokens.container.danger.focus.base.borderColor};
-      }
-    `}
-
-  .${prefix}__menu-list {
-    ${scrollbarStyling}
-  }
-  &:hover
-    .${prefix}__dropdown-indicator,
-    &:focus-within
-    .${prefix}__dropdown-indicator {
-    ${tokens.dropdownIndicator.hover.base}
-  }
-
-  ${({ isDisabled }) =>
-    isDisabled &&
-    css`
-      cursor: not-allowed;
-      ${tokens.container.disabled.base}
-      &:hover {
-        box-shadow: none;
-        ${tokens.container.disabled.base}
-        ${Label} {
-          ${tokens.label.base}
-        }
-        &:hover .${prefix}__dropdown-indicator {
-          ${tokens.dropdownIndicator.base}
-        }
-      }
-    `}
-  ${({ readOnly }) =>
-    readOnly &&
-    css`
-      ${tokens.container.readOnly.base}
-      &:hover {
-        box-shadow: none;
-        ${tokens.container.readOnly.base}
-        ${Label} {
-          ${tokens.label.base}
-        }
-      }
-      .${prefix}__dropdown-indicator, &:hover .${prefix}__dropdown-indicator {
-        ${tokens.dropdownIndicator.readOnly.base}
-      }
-    `}
-`;
-
-const Wrapper = styled.div<{ width?: CSS.WidthProperty<string> }>`
-  margin: 0;
-  box-sizing: border-box;
-  width: ${({ width }) => width};
-`;
-
-const SelectedIconWrapper = styled(IconWrapper)`
-  margin: ${tokens.option.selected.icon.margin};
-`;
 
 const { Option: DdsOption, NoOptionsMessage } = components;
 
@@ -140,101 +27,13 @@ const IconOption = (props: OptionProps<any, any>) => (
     {props.isSelected && (
       <SelectedIconWrapper Icon={CheckOutlinedIcon} iconSize="inline" />
     )}
-    {props.data.label}
+    {props.children}
   </DdsOption>
 );
 
 const NoOptionsMessageCustom = (props: NoticeProps<any, any>) => (
   <NoOptionsMessage {...props}>Ingen treff</NoOptionsMessage>
 );
-
-const customStyles: Partial<Styles<any, false, any>> = {
-  placeholder: () => {
-    return {
-      ...tokens.placeholder.base
-    };
-  },
-  indicatorSeparator: () => {
-    return {
-      backgroundColor: 'transparent'
-    };
-  },
-  dropdownIndicator: (provided, state) => {
-    return {
-      display: 'flex',
-      transform: state.selectProps.menuIsOpen ? 'rotate(180deg)' : '',
-      transition: '0.2s',
-      ...tokens.dropdownIndicator.base
-    };
-  },
-  control: () => {
-    return {
-      position: 'relative',
-      display: 'flex',
-      flexWrap: 'wrap',
-      ...tokens.input.base
-    };
-  },
-  valueContainer: provided => {
-    return {
-      ...provided,
-      padding: 0
-    };
-  },
-  singleValue: provided => {
-    return {
-      ...provided,
-      margin: 0
-    };
-  },
-  menu: () => {
-    return {
-      position: 'relative',
-      boxSizing: 'border-box',
-      zIndex: 3,
-      transition: '0.2s',
-      width: 'calc(100% + 2px)',
-      transform: 'translate(-1px)',
-      top: tokens.optionsList.spaceTop,
-      boxShadow: `0 0 0 1px ${tokens.optionsList.base.borderColor}`,
-      ...tokens.optionsList.base
-    };
-  },
-  option: (provided, state) => {
-    let selectedBase: CSSObject = {};
-    let focusedBase: CSSObject = {};
-    if (state.isSelected) selectedBase = { ...tokens.option.selected.base };
-    if (state.isFocused) focusedBase = { ...tokens.option.selected.base };
-    return {
-      display: 'flex',
-      alignItems: 'center',
-      transition: '0.2s',
-      ...tokens.option.base,
-      '&:hover': {
-        ...tokens.option.hover.base
-      },
-      ...selectedBase,
-      ...focusedBase
-    };
-  },
-  noOptionsMessage: () => {
-    return {
-      ...tokens.noOptionsMessage.base
-    };
-  },
-  clearIndicator: () => {
-    return {
-      display: 'flex',
-      ...tokens.clearIndicator.base,
-      '&:hover': {
-        ...tokens.clearIndicator.hover.base
-      }
-    };
-  },
-  input: () => {
-    return {};
-  }
-};
 
 function escapeRegexCharacters(text: string) {
   return text.replace(/[-[\]{}()*+?.,\\^$|#\s]/g, '\\$&');
@@ -248,6 +47,12 @@ export function searchFilter(text: string, search: string): boolean {
   return searchFilterRegex.test(text.toLowerCase());
 }
 
+export type SelectOption = {
+  label: string;
+  value: string | number;
+  data?: unknown;
+};
+
 export type SelectProps = {
   label?: string;
   required?: boolean;
@@ -257,12 +62,11 @@ export type SelectProps = {
   width?: CSS.WidthProperty<string>;
   className?: string;
   style?: React.CSSProperties;
-} & ReactSelectProps &
-  HTMLAttributes<HTMLDivElement>;
+} & ReactSelectProps<SelectOption>;
 
 let nextUniqueId = 0;
 
-export const Select = forwardRef<HTMLDivElement, SelectProps>(
+export const Select = forwardRef<ReactSelect<SelectOption>, SelectProps>(
   (
     {
       id,
@@ -271,7 +75,9 @@ export const Select = forwardRef<HTMLDivElement, SelectProps>(
       tip,
       required,
       readOnly,
+      options,
       value,
+      defaultValue,
       width = tokens.container.defaultWidth,
       className,
       style,
@@ -284,6 +90,10 @@ export const Select = forwardRef<HTMLDivElement, SelectProps>(
   ) => {
     const [uniqueId] = useState<string>(id ?? `select-${nextUniqueId++}`);
 
+    const wrapperProps = {
+      width
+    };
+
     const containerProps = {
       errorMessage,
       isDisabled,
@@ -293,21 +103,18 @@ export const Select = forwardRef<HTMLDivElement, SelectProps>(
       style
     };
 
-    const wrapperProps = {
-      width,
-      ...rest
-    };
-
-    const reactSelectProps: ReactSelectProps = {
+    const reactSelectProps: ReactSelectProps<SelectOption> = {
+      options: options,
+      value: value,
+      defaultValue: defaultValue,
       isDisabled: isDisabled || readOnly,
-      isClearable: value ? false : isClearable,
-      value,
-      placeholder,
+      isClearable: isClearable,
+      placeholder: placeholder,
       inputId: uniqueId,
       name: uniqueId,
       classNamePrefix: prefix,
-      styles: customStyles,
-      filterOption: (option: Option, inputValue: string) => {
+      styles: CustomStyles,
+      filterOption: (option: SelectOption, inputValue: string) => {
         const { label } = option;
         return searchFilter(label, inputValue) || inputValue === '';
       },
@@ -319,7 +126,7 @@ export const Select = forwardRef<HTMLDivElement, SelectProps>(
     };
 
     return (
-      <Wrapper ref={ref} {...wrapperProps}>
+      <Wrapper {...wrapperProps}>
         <Container {...containerProps}>
           {label && (
             <Label
@@ -330,14 +137,15 @@ export const Select = forwardRef<HTMLDivElement, SelectProps>(
               {label} {required && <RequiredMarker />}
             </Label>
           )}
-          <ReactSelect {...reactSelectProps} />
+          <ReactSelect {...reactSelectProps} ref={ref} />
         </Container>
-        {errorMessage ? (
+
+        {errorMessage && (
           <InputMessage messageType={'error'} message={errorMessage} />
-        ) : tip ? (
+        )}
+
+        {tip && !errorMessage && (
           <InputMessage messageType={'tip'} message={tip} />
-        ) : (
-          ''
         )}
       </Wrapper>
     );
