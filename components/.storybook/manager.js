92502f6fc7178d99df5af7ab05d000da4d4bdcac
Agata Jedryszek <agata.jedryszek@bouvet.no>
2021-11-18 11:44:49 +0100

    imported font-face CSS file into Storybook

--- components/.storybook/manager.js
+++ components/.storybook/manager.js
@@ -1,6 +1,7 @@
+import '../src/styles/fontStyles.css';
 import { addons } from '@storybook/addons';
 import ddsTheme from './ddsTheme';
 
 addons.setConfig({
-  theme: ddsTheme,
+  theme: ddsTheme
 });
--- components/.storybook/preview.js
+++ components/.storybook/preview.js
@@ -1,11 +1,14 @@
+import '../src/styles/fontStyles.css';
 
 export const parameters = {
-  actions: { argTypesRegex: "^on[A-Z].*" },
+  actions: { argTypesRegex: '^on[A-Z].*' },
   options: {
     storySort: (a, b) => {
-      if(a[1].id === 'design-system-introduction--page') return -1;
-      if(b[1].id === 'design-system-introduction--page') return 1;
-      return a[1].kind === b[1].kind ? 0 : a[1].id.localeCompare(b[1].id, undefined, { numeric: true });
+      if (a[1].id === 'design-system-introduction--page') return -1;
+      if (b[1].id === 'design-system-introduction--page') return 1;
+      return a[1].kind === b[1].kind
+        ? 0
+        : a[1].id.localeCompare(b[1].id, undefined, { numeric: true });
     }
-  },
-}
+  }
+};
