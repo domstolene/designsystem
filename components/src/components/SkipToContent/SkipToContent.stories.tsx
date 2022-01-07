611fcec44f7c33642093ad010f0250456c1724e7
Agata Jedryszek <agata.jedryszek@bouvet.no>
2022-01-07 13:58:28 +0100

    fiexed SkipToContent stories

--- components/src/components/SkipToContent/SkipToContent.stories.tsx
+++ components/src/components/SkipToContent/SkipToContent.stories.tsx
@@ -1,18 +1,20 @@
-import { SkipToContent } from '.';
 import { StoryTemplate } from '../../storybook/StoryTemplate';
+import { ddsBaseTokens } from '@norges-domstoler/dds-design-tokens';
+import { SkipToContent, SkipToContentProps } from '.';
+import { Typography } from '../Typography';
 
-import {
-  SkipToContent as DDSSkipToContent,
-  SkipToContentProps
-} from './SkipToContent';
+const { colors: Colors } = ddsBaseTokens;
 
 export default {
   title: 'Design system/SkipToContent',
-  component: DDSSkipToContent,
-  argTypes: {},
+  component: SkipToContent,
+  argTypes: {
+    text: { control: { type: 'text' } },
+    top: { control: { type: 'text' } }
+  },
   parameters: {
     controls: {
-      exclude: ['style', 'className', 'buttonProps']
+      exclude: ['style', 'className', 'href']
     }
   }
 };
@@ -20,7 +22,41 @@ export default {
 export const Overview = () => (
   <StoryTemplate title="Skip to content - overview">
     <div style={{ position: 'relative' }}>
-      <SkipToContent href="#" />
+      <SkipToContent href="#innhold" />
+      <SkipToContent href="#innhold" top={'30px'} text="Alternativ tekst" />
+      'Tab' for å se komponenten
+      <Typography id="innhold">Innhold</Typography>
+    </div>
+  </StoryTemplate>
+);
+
+export const Default = (args: SkipToContentProps) => (
+  <StoryTemplate title="Skip to content - default">
+    <div style={{ position: 'relative' }}>
+      <SkipToContent href="#innhold" {...args} />
+      'Tab' for å se komponenten
+      <Typography id="innhold">Innhold</Typography>
+    </div>
+  </StoryTemplate>
+);
+
+export const Example = (args: SkipToContentProps) => (
+  <StoryTemplate title="Skip to content - example">
+    <div style={{ position: 'relative' }}>
+      <SkipToContent href="#innhold" {...args} />
+      'Tab' for å se komponenten
+      <Typography typographyType="headingSans08" withMargins>
+        Placeholder
+      </Typography>
+      <div
+        style={{
+          height: '800px',
+          backgroundColor: Colors.DdsColorPrimaryLightest
+        }}
+      ></div>
+      <Typography id="innhold" withMargins>
+        Innhold
+      </Typography>
     </div>
   </StoryTemplate>
 );
