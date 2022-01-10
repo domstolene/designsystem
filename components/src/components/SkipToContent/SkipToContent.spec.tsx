fde9994574cc77e8d5a5449e9de799ad566a092e
Agata Jedryszek <agata.jedryszek@bouvet.no>
2022-01-10 14:25:26 +0100

    added SkipToContent tests

--- components/src/components/SkipToContent/SkipToContent.spec.tsx
+++ components/src/components/SkipToContent/SkipToContent.spec.tsx
@@ -1,3 +1,18 @@
 import '@testing-library/jest-dom';
 import { render, screen } from '@testing-library/react';
 import { SkipToContent } from '.';
+
+describe('<SkipToContent />', () => {
+  it('should have a link in navigation', () => {
+    const href = '#';
+    const { container } = render(<SkipToContent href={href} />);
+    const link = container.querySelector('div')?.querySelector('a');
+    expect(link).toHaveAttribute('href', href);
+  });
+  it('should display provided text', () => {
+    const text = 'text';
+    render(<SkipToContent href="#" text={text} />);
+    const labelElement = screen.getByText(text);
+    expect(labelElement).toBeDefined();
+  });
+});
