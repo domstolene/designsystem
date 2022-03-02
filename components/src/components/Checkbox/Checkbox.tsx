5c6d16fdd7a026e4c9b3bbf14345bba3ec9b2e6e
Agata Jedryszek <agata.jedryszek@bouvet.no>
2022-03-02 13:36:11 +0100

    tip support in CheckboxGroup + ARIA

--- components/src/components/Checkbox/Checkbox.tsx
+++ components/src/components/Checkbox/Checkbox.tsx
@@ -2,6 +2,7 @@ import { forwardRef, useState } from 'react';
 import { Typography } from '../Typography';
 import { CustomCheckbox, Input, Container } from './Checkbox.styles';
 import { CheckboxProps } from './Checkbox.types';
+import { useCheckboxGroup } from './CheckboxGroupContext';
 
 let nextUniqueId = 0;
 
@@ -15,6 +16,7 @@ export const Checkbox = forwardRef<HTMLInputElement, CheckboxProps>(
       disabled,
       readOnly,
       indeterminate,
+      'aria-describedby': ariaDescribedby,
       className,
       style,
       ...rest
@@ -22,9 +24,10 @@ export const Checkbox = forwardRef<HTMLInputElement, CheckboxProps>(
     ref
   ) => {
     const [uniqueId] = useState<string>(id ?? `checkbox-${nextUniqueId++}`);
+    const checkboxGroup = useCheckboxGroup();
 
     const containerProps = {
-      error,
+      error: error || checkboxGroup?.error,
       disabled,
       indeterminate,
       readOnly,
@@ -34,22 +37,31 @@ export const Checkbox = forwardRef<HTMLInputElement, CheckboxProps>(
       style
     };
 
+    const describedbyIds = [];
+    if (checkboxGroup?.tipId) describedbyIds.push(checkboxGroup?.tipId);
+    if (checkboxGroup?.errorMessageId)
+      describedbyIds.push(checkboxGroup?.errorMessageId);
+    if (ariaDescribedby) describedbyIds.push(ariaDescribedby);
+    type AriaChecked = 'mixed' | boolean | undefined;
+
     const inputProps = {
       ref,
       id: uniqueId,
       name,
       indeterminate,
       disabled: disabled || readOnly,
+      'aria-describedby':
+        describedbyIds.length > 0 ? describedbyIds.join(' ') : undefined,
+      'aria-invalid': error || checkboxGroup?.error ? true : undefined,
+      'aria-labelledby': checkboxGroup?.uniqueGroupId,
+      'aria-checked': indeterminate ? ('mixed' as AriaChecked) : undefined,
+      'aria-readonly': readOnly,
       ...rest
     };
 
     return (
       <Container {...containerProps}>
-        <Input
-          {...inputProps}
-          data-indeterminate={indeterminate}
-          aria-readonly={readOnly}
-        />
+        <Input {...inputProps} data-indeterminate={indeterminate} />
         <CustomCheckbox />
         {label && <Typography as="span">{label}</Typography>}
       </Container>
