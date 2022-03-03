a7ea5825e3951ef271adc54103d38c75e1e4e76b
Agata Jedryszek <agata.jedryszek@bouvet.no>
2022-03-03 11:10:23 +0100

    checking for undefined in id generators

--- components/src/utils/idGenerator.tsx
+++ components/src/utils/idGenerator.tsx
@@ -4,18 +4,20 @@ export const derivativeIdGenerator = (
   value?: string | number
 ) => (value ? `${prefix}-${suffix}` : undefined);
 
-export const idArrayGenerator = (values: (string | undefined)[]): string[] => {
+export const idArrayGenerator = (
+  values: (string | undefined)[]
+): string[] | undefined => {
   const array: string[] = [];
   values.forEach(e => {
     if (e) {
       array.push(e);
     }
   });
-  return array;
+  return array.length > 0 ? array : undefined;
 };
 
 export const spaceSeparatedIdListGenerator = (
   values: (string | undefined)[]
-): string => {
-  return idArrayGenerator(values).join(' ');
+): string | undefined => {
+  return idArrayGenerator(values)?.join(' ');
 };
