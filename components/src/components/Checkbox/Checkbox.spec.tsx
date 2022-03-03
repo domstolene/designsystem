d1fe542875dedfb9efc7c67203aa25c055779eed
Agata Jedryszek <agata.jedryszek@bouvet.no>
2022-03-03 14:02:45 +0100

    updated selection controls tests

--- components/src/components/Checkbox/Checkbox.spec.tsx
+++ components/src/components/Checkbox/Checkbox.spec.tsx
@@ -1,6 +1,6 @@
 import '@testing-library/jest-dom';
 import { render, screen } from '@testing-library/react';
-import { Checkbox } from './Checkbox';
+import { Checkbox, CheckboxGroup } from '.';
 
 describe('<Checkbox />', () => {
   it('should have a label', () => {
@@ -24,5 +24,29 @@ describe('<Checkbox />', () => {
     expect(checkbox).not.toBeChecked();
     checkbox?.click();
     expect(checkbox).not.toBeChecked();
+  });
+  it('children of CheckboxGroup should have aria-describedby as tip id when tip provided', () => {
+    const groupId = 'groupId';
+    render(
+      <CheckboxGroup groupId={groupId} tip="tip">
+        <Checkbox id="test" label="Test" />
+      </CheckboxGroup>
+    );
+    const checkbox = screen.getByRole('checkbox');
+    expect(checkbox).toHaveAttribute('aria-describedby', `${groupId}-tip`);
+  });
+  it('children of CheckboxGroup should have aria-describedby as errorMessage id and aria-invalid when errorMessage provided', () => {
+    const groupId = 'groupId';
+    render(
+      <CheckboxGroup groupId={groupId} errorMessage="errorMessage">
+        <Checkbox id="test" label="Test" />
+      </CheckboxGroup>
+    );
+    const checkbox = screen.getByRole('checkbox');
+    expect(checkbox).toHaveAttribute(
+      'aria-describedby',
+      `${groupId}-errorMessage`
+    );
+    expect(checkbox).toHaveAttribute('aria-invalid', 'true');
   });
 });
