25a1a8d8927e91f2091f0b9338033eedf1557ae2
Agata Jedryszek <agata.jedryszek@bouvet.no>
2022-03-04 15:49:31 +0100

    added removeButtonStyling helper

---
+++ components/src/helpers/styling/removeButtonStyling.tsx
@@ -0,0 +1,10 @@
+import { css } from 'styled-components';
+export const removeButtonStyling = css`
+  background: none;
+  color: inherit;
+  border: none;
+  padding: 0;
+  font: inherit;
+  cursor: pointer;
+  outline: inherit;
+`;
