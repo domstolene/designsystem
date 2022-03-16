b2602c52def197529864e30ac71f897bb4ae9c17
Agata Jedryszek <agata.jedryszek@bouvet.no>
2022-03-16 12:28:04 +0100

    refactored animated chevron

--- components/src/helpers/Chevron/AnimatedChevronUpDown.tsx
+++ components/src/helpers/Chevron/AnimatedChevronUpDown.tsx
@@ -1,22 +1,32 @@
 import {
-  SvgChevronProps,
   SvgChevronlRight,
   SvgChevronlLeft,
   SvgChevronlGroup,
   SvgChevron
 } from './AnimatedChevronUpDown.styles';
+import * as CSS from 'csstype';
 
 const svgChevronLeftPath =
   'M 1 0 L 5 4 C 5 4 5 4 5 4 L 4 5 C 4 5 4 5 4 5 C 4 5 4 5 4 5 L 0 1 C 0 1 0 1 0 1 L 1 0 C 1 0 1 0 1 0 C 1 0 1 0 1 0 L 1 0 Z';
 const svgChevronRightPath =
   'M 3 4 L 7 0 L 8 1 C 8 1 8 1 8 1 L 4 5 L 3 4 C 3 4 3 4 3 4 L 3 4 Z';
 
-export const AnimatedChevronUpDown = ({ isUp }: SvgChevronProps) => {
+type SvgChevronProps = {
+  isUp?: boolean;
+  height?: CSS.HeightProperty<string>;
+  width?: CSS.WidthProperty<string>;
+};
+
+export const AnimatedChevronUpDown = ({
+  isUp,
+  height = '5px',
+  width = '8px'
+}: SvgChevronProps) => {
   return (
     <SvgChevron
       viewBox="0 0 8 5"
-      width="8px"
-      height="5px"
+      width={width}
+      height={height}
       fill="currentColor"
       isUp={isUp}
     >
