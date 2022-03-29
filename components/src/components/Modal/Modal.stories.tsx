35d3989af969ac64fc49972258d509d5a179a6bb
Agata Jedryszek <agata.jedryszek@bouvet.no>
2022-03-29 15:27:57 +0200

    updated Modal stories

--- components/src/components/Modal/Modal.stories.tsx
+++ components/src/components/Modal/Modal.stories.tsx
@@ -11,26 +11,43 @@ export default {
 export const Overview = (args: ModalProps) => {
   const [closed, setClosed] = useState(true);
   const show = () => setClosed(false);
-  const close = () => setClosed(true);
+  const close = () => {
+    setClosed(true);
+    buttonRef.current?.focus();
+  };
 
   const [closed2, setClosed2] = useState(true);
   const show2 = () => setClosed2(false);
-  const close2 = () => setClosed2(true);
+  const close2 = () => {
+    setClosed2(true);
+    buttonRef2.current?.focus();
+  };
 
-  const ref = useRef<HTMLDivElement>(null);
+  const buttonRef = useRef<HTMLButtonElement>(null);
+  const buttonRef2 = useRef<HTMLButtonElement>(null);
 
   return (
     <StoryTemplate title="Modal - overview">
-      <Button aria-haspopup="dialog" label="Åpne" onClick={show} />
-      <Modal isOpen={!closed} onClose={close} title="Tittel">
+      <Button
+        aria-haspopup="dialog"
+        label="Åpne"
+        onClick={show}
+        ref={buttonRef}
+      />
+      <Modal isOpen={!closed} onClose={close} header="Tittel">
         <ModalBody>Lukkbar modal</ModalBody>
         <ModalControls>
           <Button label="OK" onClick={close} />
           <Button purpose="secondary" label="Avbryt" onClick={close} />
         </ModalControls>
       </Modal>
-      <Button aria-haspopup="dialog" label="Åpne" onClick={show2} />
-      <Modal isOpen={!closed2} title="Tittel">
+      <Button
+        aria-haspopup="dialog"
+        label="Åpne"
+        onClick={show2}
+        ref={buttonRef2}
+      />
+      <Modal isOpen={!closed2} header="Tittel">
         <ModalBody>Ikke lukkbar modal</ModalBody>
         <ModalControls>
           <Button label="OK" onClick={close2} />
