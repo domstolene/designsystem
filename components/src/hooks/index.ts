c1536aebc48e0bac6e16a4ba4e6dbe3a2ba7d228
Agata Jedryszek <agata.jedryszek@bouvet.no>
2022-03-29 15:02:04 +0200

    exported useFocusTrap and useMountTransition hooks

--- components/src/hooks/index.ts
+++ components/src/hooks/index.ts
@@ -3,3 +3,5 @@ export * from './useOnClickOutside';
 export * from './useOnKeyDown';
 export * from './useReactPopper';
 export * from './useRoveFocus';
+export * from './useFocusTrap';
+export * from './useMountTransition';
