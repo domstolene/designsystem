9ac1e708fcfe59c5d5c9ec7cce3df5a8be14a4b8
Agata Jedryszek <agata.jedryszek@bouvet.no>
2022-03-30 12:29:12 +0200

    added scrollable prop in ModalBody

--- components/src/components/Modal/ModalBody.tsx
+++ components/src/components/Modal/ModalBody.tsx
@@ -1,9 +1,24 @@
 import { forwardRef, HTMLAttributes } from 'react';
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
+import { modalTokens as tokens } from './Modal.tokens';
+import scrollbarStyling from '../../helpers/scrollbarStyling';
 
-const Container = styled.div``;
+type ContainerProps = {
+  scrollable?: boolean;
+};
 
-export type ModalBodyProps = {} & HTMLAttributes<HTMLDivElement>;
+const Container = styled.div<ContainerProps>`
+  ${scrollbarStyling}
+  ${({ scrollable }) =>
+    scrollable &&
+    css`
+      ${tokens.bodyScrollable.base}
+    `}
+`;
+
+export type ModalBodyProps = {
+  scrollable?: boolean;
+} & HTMLAttributes<HTMLDivElement>;
 
 export const ModalBody = forwardRef<HTMLDivElement, ModalBodyProps>(
   ({ children, ...rest }, ref) => {
