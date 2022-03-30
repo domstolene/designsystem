e1549dcd29670e96b5e82a16e6a0077c1986ed76
Agata Jedryszek <agata.jedryszek@bouvet.no>
2022-03-30 13:49:44 +0200

    changed styling logic for Backdrop when scrolling

--- components/src/helpers/Backdrop/Backdrop.utils.tsx
+++ components/src/helpers/Backdrop/Backdrop.utils.tsx
@@ -1,17 +1,14 @@
-import { getScrollbarSize } from '../../utils';
-
 const isVerticalScrollbarDisplayed = (container: HTMLElement) =>
   container.clientHeight > window.innerHeight;
 
 export function handleElementWithBackdropMount(container: HTMLElement) {
   if (isVerticalScrollbarDisplayed(container)) {
-    const scrollbarSize = getScrollbarSize(container.ownerDocument);
-    container.style.paddingRight = `${scrollbarSize}px`;
-    container.style.overflow = 'hidden';
+    container.style.position = 'fixed';
+    container.style.overflowY = 'scroll';
   }
 }
 
 export function handleElementWithBackdropUnmount(container: HTMLElement) {
-  container.style.removeProperty('padding-right');
-  container.style.removeProperty('overflow');
+  container.style.removeProperty('position');
+  container.style.removeProperty('overflow-y');
 }
