c3b6a632ce0880260e8d2cb6f073e3e206ec4d8e
Agata Jedryszek <agata.jedryszek@bouvet.no>
2022-04-08 13:43:02 +0200

    fixed useId return value

--- components/src/hooks/useId.tsx
+++ components/src/hooks/useId.tsx
@@ -4,5 +4,5 @@ let nextId = 0;
 
 export function useId(prefix: string, suffix?: string) {
   const [id] = useState(() => nextId++);
-  return `${prefix}-${id}${suffix && '-' + suffix}`;
+  return `${prefix}-${id}${suffix ? '-' + suffix : ''}`;
 }
