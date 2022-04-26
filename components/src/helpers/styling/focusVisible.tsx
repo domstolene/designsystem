4d886f429df0d17a2cf813ebfb180c5381c24cca
Agata Jedryszek <agata.jedryszek@bouvet.no>
2022-04-26 12:59:06 +0200

    added danger, hover and focus common styles

--- components/src/helpers/styling/focusVisible.tsx
+++ components/src/helpers/styling/focusVisible.tsx
@@ -1,8 +1,44 @@
 import { ddsBaseTokens } from '@norges-domstoler/dds-design-tokens';
 
-const { border: Border } = ddsBaseTokens;
+const { border: Border, colors: Colors } = ddsBaseTokens;
+
+export const outlineOffset = '2px';
 
 export const focusVisible = {
   outline: `${Border.BordersDdsBorderFocusBaseStrokeWeight} ${Border.BordersDdsBorderFocusBaseStroke} solid`,
-  outlineOffset: '1px'
+  outlineOffset: outlineOffset
+};
+
+export const focusVisibleOnDark = {
+  outline: `${Border.BordersDdsBorderFocusBaseStrokeWeight} ${Colors.DdsColorWarningLighter} solid`,
+  outlineOffset: outlineOffset
+};
+
+export const focusVisibleLink = {
+  outline: 'none',
+  backgroundColor: Border.BordersDdsBorderFocusBaseStroke,
+  color: Colors.DdsColorNeutralsWhite
+};
+
+export const focusVisibleLinkOnDark = {
+  outline: 'none',
+  backgroundColor: Colors.DdsColorWarningLighter,
+  color: Colors.DdsColorNeutralsGray9
+};
+
+export const focusVisibleWithBorder = {
+  outline: 'none',
+  borderColor: Border.BordersDdsBorderFocusCardStroke,
+  boxShadow: `inset 0 0 0 1px ${Border.BordersDdsBorderFocusCardStroke}`
+};
+
+export const focusVisibleThickWithBorderOnDark = {
+  outline: 'none',
+  borderColor: Colors.DdsColorWarningLighter,
+  boxShadow: `inset 0 0 0 1px ${Colors.DdsColorWarningLighter}`
+};
+
+export const focusVisibleInset = {
+  outline: 'none',
+  boxShadow: `inset 0 0 0 2px ${Border.BordersDdsBorderFocusCardStroke}`
 };
--- components/src/helpers/styling/index.tsx
+++ components/src/helpers/styling/index.tsx
@@ -1,2 +1,6 @@
 export * from './removeButtonStyling';
 export * from './visibilityTransition';
+export * from './focusVisible';
+export * from './hover';
+export * from './focus';
+export * from './danger';
