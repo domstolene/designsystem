3d2beef748e31a69d64024940b2a926c04e27f41
Endre Waatevik <endwaat@gmail.com>
2022-04-26 12:22:27 +0200

    Fix incompatible types of 'ref' for Card
    * HTMLDivElement and HTMLAnchorElement does not have compatible attributes
    * 'cardType'-prop is not optional anymore

--- components/src/components/Card/Card.spec.tsx
+++ components/src/components/Card/Card.spec.tsx
@@ -4,7 +4,7 @@ import { Card } from '.';
 describe('<Card />', () => {
   it('should render content text', () => {
     const text = 'text';
-    render(<Card>{text}</Card>);
+    render(<Card cardType="info">{text}</Card>);
     expect(screen.queryByText(text)).toBeDefined;
   });
   it('should render an anchor element when cardType is navigation and href is provided', () => {
