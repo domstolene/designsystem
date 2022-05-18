18e3451e32ceed3479cc9aebbf0263b1dabcc6c0
Agata Jedryszek <agata.jedryszek@bouvet.no>
2022-05-18 13:13:44 +0200

    imported CSS design tokens into correct storybook files

--- components/.storybook/manager.js
+++ components/.storybook/manager.js
@@ -1,4 +1,6 @@
 import '../src/styles/fontStyles.css';
+import '@norges-domstoler/dds-design-tokens/dist/css/colors.css';
+import '@norges-domstoler/dds-design-tokens/dist/css/font.css';
 import { addons } from '@storybook/addons';
 import ddsTheme from './ddsTheme';
 
