10daed842a88cb0adddf804e4789ee12cbcf0fca
Agata Jedryszek <agata.jedryszek@bouvet.no>
2022-06-03 15:57:43 +0200

    added useScreenSize hook

---
+++ components/src/hooks/useScreenSize.tsx
@@ -0,0 +1,55 @@
+import { useEffect, useState } from 'react';
+import { ddsBaseTokens } from '@norges-domstoler/dds-design-tokens';
+
+const { breakpoints } = ddsBaseTokens;
+
+export enum ScreenSize {
+  XSmall = 0,
+  Small,
+  Medium,
+  Large,
+  XLarge
+}
+
+const screenSizesAndMediaQueries: [ScreenSize, string][] = [
+  [
+    ScreenSize.XSmall,
+    `only screen and (min-width: ${breakpoints.DdsBreakpointXs})`
+  ],
+  [
+    ScreenSize.Small,
+    `only screen and (min-width: ${breakpoints.DdsBreakpointSm})`
+  ],
+  [
+    ScreenSize.Medium,
+    `only screen and (min-width: ${breakpoints.DdsBreakpointMd})`
+  ],
+  [
+    ScreenSize.Large,
+    `only screen and (min-width: ${breakpoints.DdsBreakpointLg})`
+  ],
+  [
+    ScreenSize.XLarge,
+    `only screen and (min-width: ${breakpoints.DdsBreakpointXl})`
+  ]
+];
+
+export const useScreenSize = function () {
+  const [screenSize, setScreenSize] = useState(ScreenSize.Large);
+
+  useEffect(() => {
+    function listener() {
+      for (const [screenSize, mediaQuery] of screenSizesAndMediaQueries) {
+        if (window.matchMedia(mediaQuery).matches) {
+          setScreenSize(screenSize);
+          return;
+        }
+      }
+    }
+
+    window.addEventListener('resize', listener);
+    return () => window.removeEventListener('resize', listener);
+  }, []);
+
+  return screenSize;
+};
