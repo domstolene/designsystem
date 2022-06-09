28a3b68e40143348ae7661fafaf0906eae5aa43f
Agata Jedryszek <agata.jedryszek@bouvet.no>
2022-06-09 14:49:15 +0200

    fix Button changing width when loading

--- components/src/components/Button/Button.tsx
+++ components/src/components/Button/Button.tsx
@@ -27,6 +27,9 @@ export const Button = forwardRef<HTMLButtonElement, ButtonProps>(
   ) => {
     const as: ElementType = href ? 'a' : 'button';
 
+    const hasLabel = !!label;
+    const hasIcon = !!Icon;
+
     const wrapperProps = {
       href,
       label,
@@ -38,8 +41,8 @@ export const Button = forwardRef<HTMLButtonElement, ButtonProps>(
       purpose,
       iconPosition,
       fullWidth,
-      hasLabel: !!label,
-      hasIcon: !!Icon,
+      hasLabel: hasLabel,
+      hasIcon: hasIcon,
       isLoading: loading,
       disabled,
       size,
@@ -48,13 +51,13 @@ export const Button = forwardRef<HTMLButtonElement, ButtonProps>(
       ...rest
     };
 
-    const hasLabel = !!label;
-    const isIconButton = !hasLabel && !!Icon;
+    const isIconButton = !hasLabel && hasIcon;
 
     const iconElement = Icon && (
       <StyledIconWrapperSpan
         iconPosition={isIconButton ? undefined : iconPosition}
         size={size}
+        isHidden={hasIcon && loading}
       >
         <IconWrapper Icon={Icon} iconSize="inline" />
       </StyledIconWrapperSpan>
@@ -62,23 +65,26 @@ export const Button = forwardRef<HTMLButtonElement, ButtonProps>(
 
     return (
       <ButtonWrapper {...wrapperProps}>
+        {!isIconButton && (
+          <>
+            {iconPosition === 'left' && iconElement}
+            <Label isHidden={loading} aria-hidden={loading}>
+              {label}
+            </Label>
+            {iconPosition === 'right' && iconElement}
+          </>
+        )}
+        {isIconButton && iconElement}
         {loading && (
-          <StyledIconWrapperSpan size={size}>
+          <StyledIconWrapperSpan
+            size={size}
+            absolutePosition={hasIcon || hasLabel}
+          >
             <Spinner
               color={tokens.appearance[appearance][purpose].base.color}
               size={tokens.sizes[size].justIcon.base.fontSize as string}
             />
           </StyledIconWrapperSpan>
-        )}
-
-        {isIconButton && !loading && iconElement}
-
-        {hasLabel && !loading && (
-          <>
-            {iconPosition === 'left' && iconElement}
-            <Label>{label}</Label>
-            {iconPosition === 'right' && iconElement}
-          </>
         )}
       </ButtonWrapper>
     );
