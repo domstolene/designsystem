47d3b0f2a5eb669454421a837a1787cfd435d7da
Agata Jedryszek <agata.jedryszek@bouvet.no>
2022-08-16 10:29:12 +0200

    fikset ikonnavn typo

--- components/src/icons/tsx/questionAnswer.tsx
+++ components/src/icons/tsx/questionAnswer.tsx
@@ -1,6 +1,6 @@
 import { StyledSvg } from '../utils/StyledSvg';
 import { SvgProps } from '../utils/propTypes';
-export function QestionAnswerIcon(props: SvgProps) {
+export function QuestionAnswerIcon(props: SvgProps) {
   const { title, ...rest } = props;
   return (
     <StyledSvg
