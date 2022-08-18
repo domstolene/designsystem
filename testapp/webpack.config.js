3e4fc13dfe61d4c1d36635b3131f8bbe6a798b23
Petter Sæther Moen <pettersmoen@pm.me>
2022-08-18 10:34:03 +0200

    sett opp dev server for testapp

--- testapp/webpack.config.js
+++ testapp/webpack.config.js
@@ -1,12 +1,21 @@
+const HtmlWebPackPlugin = require('html-webpack-plugin')
 const path = require('path');
 
 module.exports = {
   mode: 'development',
-  entry: { main: './index.tsx' },
+  entry: { main: './src/index.tsx' },
   devtool: false,
   resolve: {
     extensions: ['.ts', '.tsx', '.js', '.jsx'],
     symlinks: false,
+  },
+  devServer: {
+    historyApiFallback: true,
+    static: {
+      directory: path.join(__dirname, 'public'),
+    },
+    compress: true,
+    port: 3000,
   },
   optimization: {
     usedExports: true,
@@ -19,20 +28,27 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\.tsx?$/,
-          use: [
-            {
-              loader: 'babel-loader',
-              options: {
-                cacheDirectory: true,
-                compact: true,
-              },
+        test: /\.[tj]sx?$/,
+        use: [
+          {
+            loader: 'babel-loader',
+            options: {
+              cacheDirectory: true,
+              compact: true,
             },
-          ],
-      }
+          },
+        ],
+      },
     ]
   },
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: path.resolve(__dirname, 'public', 'index.html'),
+      filename: 'index.html',
+    })
+  ],
   output: {
-    path: path.resolve(__dirname, 'build')
+    path: path.resolve(__dirname, 'build'),
+    publicPath: '/',
   }
 }
