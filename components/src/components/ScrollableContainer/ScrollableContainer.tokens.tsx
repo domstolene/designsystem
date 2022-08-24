76bed3a52cc3bf45b0abfc8bdbf674f0ac6d9135
Agata Jedryszek <agata.jedryszek@bouvet.no>
2022-08-24 12:28:20 +0200

    legger til scrollbarWidthNumberPx

--- components/src/components/ScrollableContainer/ScrollableContainer.tokens.tsx
+++ components/src/components/ScrollableContainer/ScrollableContainer.tokens.tsx
@@ -1,18 +1,20 @@
 import { ddsBaseTokens } from '@norges-domstoler/dds-design-tokens';
 
+export const scrollbarWidthNumberPx = 10;
+const scrollbarWidth = `${scrollbarWidthNumberPx}px`;
 const { colors, spacing } = ddsBaseTokens;
 
 const track = {
   backgroundColor: 'transparent',
   borderRadius: '100px',
-  width: '10px',
+  width: scrollbarWidth,
 };
 
 const thumb = {
   base: {
     backgroundColor: colors.DdsColorPrimaryDarkest.slice(0, -2) + '0.35)',
     borderRadius: '100px',
-    width: '10px',
+    width: scrollbarWidth,
   },
   hover: {
     backgroundColor: colors.DdsColorPrimaryDarkest.slice(0, -2) + '0.5)',
