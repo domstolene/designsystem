b9f921425441da0663d888278a767346035c4ba8
Agata Jedryszek <agata.jedryszek@bouvet.no>
2022-09-06 09:52:29 +0200

    legger til flere RadioButton tester

--- components/src/components/RadioButton/RadioButton.spec.tsx
+++ components/src/components/RadioButton/RadioButton.spec.tsx
@@ -1,7 +1,54 @@
-import { render, screen } from '@testing-library/react';
+import { act, render, screen } from '@testing-library/react';
+import { ChangeEvent, useState } from 'react';
 import { RadioButton, RadioButtonGroup } from '.';
 
+const TestComponent = () => {
+  const [value, setValue] = useState<number | undefined>(1);
+
+  return (
+    <RadioButtonGroup
+      value={value}
+      onChange={(
+        _e: ChangeEvent<HTMLInputElement>,
+        value: number | undefined
+      ) => {
+        setValue(value);
+      }}
+    >
+      <RadioButton value={1} label="Test 1" />
+      <RadioButton value={2} label="Test 2" />
+      <RadioButton value={3} label="Test 3" />
+    </RadioButtonGroup>
+  );
+};
+
 describe('<RadioButton />', () => {
+  it('should set default checked state', () => {
+    const { container } = render(<TestComponent />);
+    const input = container.querySelector('input');
+    expect(input?.checked).toBeTruthy();
+  });
+
+  test('should update checked state when selecting a radio button', async () => {
+    const { container } = render(<TestComponent />);
+    const label = container.querySelectorAll('label')[2];
+    act(() => {
+      label?.click();
+    });
+    const input = label.querySelector('input');
+    expect(input?.checked).toBeTruthy();
+  });
+
+  it('should have radiogroup as role', () => {
+    render(
+      <RadioButtonGroup>
+        <RadioButton />
+      </RadioButtonGroup>
+    );
+    const radioGroup = screen.queryByRole('radiogroup');
+    expect(radioGroup).toBeInTheDocument();
+  });
+
   it('RadioButtonGroup should have aria-describedby as tip id when tip provided', () => {
     const groupId = 'groupId';
     render(
@@ -12,6 +59,7 @@ describe('<RadioButton />', () => {
     const radioGroup = screen.getByRole('radiogroup');
     expect(radioGroup).toHaveAttribute('aria-describedby', `${groupId}-tip`);
   });
+
   it('children of RadioButtonGroup should have aria-describedby as errorMessage id and aria-invalid when errorMessage provided', () => {
     const groupId = 'groupId';
     render(
