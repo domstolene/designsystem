f139ec9fec5f640be2c650ecd918ff2172210be0
Agata Jedryszek <agata.jedryszek@bouvet.no>
2022-09-12 11:06:38 +0200

    stories med nye Typography-komponenter

---
+++ components/src/components/Typography/stories/TypographyComponents.stories.tsx
@@ -0,0 +1,30 @@
+import { Label, Paragraph, Heading, Link, Legend, Caption } from '..';
+import { Typography } from '..';
+import { StoryTemplate } from '../../../storybook';
+
+export default {
+  title: 'Design system/Typography/Overview',
+  argTypes: {
+    typographyType: { control: { type: 'select' } },
+    bold: { control: { type: 'boolean' } },
+    italic: { control: { type: 'boolean' } },
+    underline: { control: { type: 'boolean' } },
+    withMargins: { control: { type: 'boolean' } },
+    color: { control: { type: 'text' } },
+    text: { control: { type: 'text' } },
+  },
+};
+
+export const ComponentsOverview = () => {
+  return (
+    <StoryTemplate title="Typography components - overview">
+      <Heading level={1}>Heading</Heading>
+      <Paragraph>Paragraph</Paragraph>
+      <Label>Label</Label>
+      <Link>Link</Link>
+      <Legend>Legend</Legend>
+      <Caption>Caption</Caption>
+      <Typography>Typography</Typography>
+    </StoryTemplate>
+  );
+};
