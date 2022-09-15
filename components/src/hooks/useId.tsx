2bd9871fde295bdc4d0f62c606c4270e92189e15
Agata Jedryszek <agata.jedryszek@bouvet.no>
2022-09-15 14:18:00 +0200

    fjerner ubrukt useId

--- components/src/hooks/useId.tsx
+++
@@ -1,8 +0,0 @@
-import { useState } from 'react';
-
-let nextId = 0;
-
-export function useId(prefix: string, suffix?: string) {
-  const [id] = useState(() => nextId++);
-  return `${prefix}-${id}${suffix ? '-' + suffix : ''}`;
-}
