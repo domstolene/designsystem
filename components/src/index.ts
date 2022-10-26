9aa379c5476ba93842422eac2a33b83ac952d914
Agata Jedryszek <agata.jedryszek@bouvet.no>
2022-10-26 08:50:08 +0200

    export SplitButton i index.ts

--- components/src/index.ts
+++ components/src/index.ts
@@ -40,3 +40,4 @@ export * from './components/Grid';
 export * from './components/ProgressTracker';
 export * from './hooks';
 export * from './components/TextArea';
+export * from './components/SplitButton';
