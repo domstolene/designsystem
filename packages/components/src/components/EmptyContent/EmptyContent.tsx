27ff0eb5e3ebe3702c7c4fed9885d65afcf73735
Endre Waatevik <endre.waatevik@bekk.no>
2023-02-10 10:19:08 +0100

    ELSA1-223 Legger til EmptyContent
    Komponent som viser en tom tilstand.

---
+++ packages/components/src/components/EmptyContent/EmptyContent.tsx
@@ -0,0 +1,43 @@
+import { ddsBaseTokens } from '@norges-domstoler/dds-design-tokens';
+import { HTMLAttributes } from 'react';
+import styled from 'styled-components';
+import { Typography } from '../Typography';
+
+const { colors, spacing } = ddsBaseTokens;
+
+const StyledEmptyContent = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  min-height: ${spacing.SizesDdsSpacingLayoutX10};
+  height: 100%;
+  width: 100%;
+  background-color: ${colors.DdsColorNeutralsGray1};
+  padding: ${spacing.SizesDdsSpacingLayoutX15};
+`;
+
+const StyledEmptyContentText = styled.div`
+  max-width: 70ch;
+  text-align: center;
+  display: flex;
+  flex-direction: column;
+  gap: ${spacing.SizesDdsSpacingLayoutX1};
+`;
+
+export type EmptyContentProps = {
+  title?: string;
+  message: string;
+} & HTMLAttributes<HTMLDivElement>;
+
+export function EmptyContent({ title, message, ...rest }: EmptyContentProps) {
+  return (
+    <StyledEmptyContent {...rest}>
+      <StyledEmptyContentText>
+        {title && (
+          <Typography typographyType="headingSans02">{title}</Typography>
+        )}
+        <Typography typographyType="bodySans02">{message}</Typography>
+      </StyledEmptyContentText>
+    </StyledEmptyContent>
+  );
+}
