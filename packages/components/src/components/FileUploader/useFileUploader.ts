43ebba91eca30a75be6dcbca00046a852f492868
brigittbright <brigittbright@gmail.com>
2023-03-14 15:03:03 +0100

    Legger til isControlled i dependecy array

--- packages/components/src/components/FileUploader/useFileUploader.ts
+++ packages/components/src/components/FileUploader/useFileUploader.ts
@@ -117,7 +117,7 @@ export const useFileUploader = <TRootElement extends HTMLElement>(
         payload: files,
       });
     }
-  }, [value]);
+  }, [value, isControlled]);
 
   useEffect(() => {
     dispatch({
