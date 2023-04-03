dfc63b533817e1f5160248f0114f7c400b18af88
Bendik Solheim <hello@bendik.dev>
2023-04-03 14:00:03 +0200

    Fjern smallScreen-variant av Breadcrumbs
    smallScreen-varianten av Breadcrumbs er nå skilt ut i en egen komponent
    BackButton. Breadcrumbs kan dermed være en renere komponent som alltid
    vises som en faktisk breadcrumb.

--- packages/components/src/components/Breadcrumbs/Breadcrumbs.stories.tsx
+++ packages/components/src/components/Breadcrumbs/Breadcrumbs.stories.tsx
@@ -4,48 +4,13 @@ import { StoryTemplate } from '../../storybook';
 export default {
   title: 'Design system/Breadcrumbs',
   component: Breadcrumbs,
-  argTypes: {
-    smallScreen: {
-      control: {
-        type: 'boolean',
-      },
-    },
-  },
-};
-
-export const Overview = (args: BreadcrumbsProps) => {
-  return (
-    <StoryTemplate title="Breadcrumbs - overview" display="grid">
-      <Breadcrumbs {...args}>
-        <Breadcrumb href="#">Side 1</Breadcrumb>
-        <Breadcrumb href="#">Side 2</Breadcrumb>
-        <Breadcrumb>Side 3</Breadcrumb>
-      </Breadcrumbs>
-      <Breadcrumbs smallScreen>
-        <Breadcrumb href="#">Side 1</Breadcrumb>
-        <Breadcrumb href="#">Side 2</Breadcrumb>
-        <Breadcrumb>Side 3</Breadcrumb>
-      </Breadcrumbs>
-    </StoryTemplate>
-  );
+  argTypes: {},
 };
 
 export const Default = (args: BreadcrumbsProps) => {
   return (
     <StoryTemplate title="Breadcrumbs - default" gap="0">
       <Breadcrumbs {...args}>
-        <Breadcrumb href="#">Side 1</Breadcrumb>
-        <Breadcrumb href="#">Side 2</Breadcrumb>
-        <Breadcrumb>Side 3</Breadcrumb>
-      </Breadcrumbs>
-    </StoryTemplate>
-  );
-};
-
-export const SmallScreen = () => {
-  return (
-    <StoryTemplate title="Breadcrumbs - small screen" gap="0">
-      <Breadcrumbs smallScreen>
         <Breadcrumb href="#">Side 1</Breadcrumb>
         <Breadcrumb href="#">Side 2</Breadcrumb>
         <Breadcrumb>Side 3</Breadcrumb>
