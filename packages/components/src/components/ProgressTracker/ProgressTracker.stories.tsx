34d37ac59984b06e76332a6b705262c70e817865
Petter Sæther Moen <pettersmoen@gmail.com>
2023-04-27 10:44:31 +0200

    Legg til story som viser hvordan disabled ProgressTracker.Item ser ut

--- packages/components/src/components/ProgressTracker/ProgressTracker.stories.tsx
+++ packages/components/src/components/ProgressTracker/ProgressTracker.stories.tsx
@@ -171,6 +171,64 @@ export const NonClickable = () => {
   );
 };
 
+export const FutureStepsDisabled = () => {
+  const numSteps = 3;
+
+  const [activeStep, setActiveStep] = useState(0);
+  const [completedSteps, setCompletedSteps] = useState(new Set<number>());
+  const highestCompletedStep = Math.max(...Array.from(completedSteps), -1);
+
+  const handleSetFinishedButtonClick = () => {
+    setCompletedSteps(s => new Set([...s, activeStep]));
+    if (activeStep < numSteps - 1) {
+      setActiveStep(s => s + 1);
+    }
+  };
+
+  const isDisabled = (index: number) => index > highestCompletedStep + 1;
+
+  return (
+    <StoryTemplate title="ProgressTracker - disabled steps" display="block">
+      <ProgressTracker
+        activeStep={activeStep}
+        htmlProps={{ style: { maxWidth: '800px' } }}
+        clickable
+        onStepChange={step => setActiveStep(step)}
+      >
+        <ProgressTracker.Item
+          disabled={isDisabled(0)}
+          completed={completedSteps.has(0)}
+        >
+          Partopplysninger
+        </ProgressTracker.Item>
+        <ProgressTracker.Item
+          disabled={isDisabled(1)}
+          completed={completedSteps.has(1)}
+        >
+          Slutning
+        </ProgressTracker.Item>
+        <ProgressTracker.Item
+          disabled={isDisabled(2)}
+          completed={completedSteps.has(2)}
+        >
+          Vedlegg
+        </ProgressTracker.Item>
+      </ProgressTracker>
+      <div style={{ margin: '10px' }}>
+        {activeStep === 0 && <div>Steg 1</div>}
+        {activeStep === 1 && <div>Steg 2</div>}
+        {activeStep === 2 && <div>Steg 3</div>}
+      </div>
+
+      <Button
+        onClick={() => activeStep > 0 && setActiveStep(s => s - 1)}
+        label="Forrige steg"
+      />
+      <Button onClick={handleSetFinishedButtonClick} label="Neste steg" />
+    </StoryTemplate>
+  );
+};
+
 export const PartiallyClickable = () => {
   const numSteps = 3;
 
