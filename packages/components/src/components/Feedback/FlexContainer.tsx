7f0ed07ee17447e3537967466ad4275218ebe45a
Petter Sæther Moen <pettersmoen@gmail.com>
2023-06-15 09:26:19 +0200

    Revert "Merge pull request #797 from domstolene/revert-feedback"
    This reverts commit 562215a6a0b88a85366ffa96d0093268d1ce0a99, reversing
    changes made to 0bba9a91dba102785d6cfd3d71f17d144099c218.

---
+++ packages/components/src/components/Feedback/FlexContainer.tsx
@@ -0,0 +1,15 @@
+import { CSSProperties } from 'react';
+import styled, { css } from 'styled-components';
+import { ddsBaseTokens } from '@norges-domstoler/dds-design-tokens';
+
+type FlexContainerProps = {
+  flexDirection: CSSProperties['flexDirection'];
+};
+
+export const FlexContainer = styled.div<FlexContainerProps>`
+  display: flex;
+  ${({ flexDirection }) => css`
+    flex-direction: ${flexDirection};
+    gap: ${ddsBaseTokens.spacing.SizesDdsSpacingLocalX1};
+  `};
+`;
