7596282ac5bdbc9d10223172f246f0f9bc24dbec
Trond Klakken <trond.klakken@nhn.no>
2023-08-24 15:27:53 +0200

    Revert "test: Disable test to see if it builds better"
    This reverts commit 1cd135aea5171b9d0e8d281ea1e1dd05ff41bbd8.

--- packages/components/src/components/Tabs/Tabs.spec.tsx
+++ packages/components/src/components/Tabs/Tabs.spec.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useRef } from 'react';
+import React, { useEffect, useRef } from 'react';
 import { render, screen, fireEvent } from '@testing-library/react';
 import { Tab, TabList, TabPanel, TabPanels, Tabs } from '.';
 
@@ -27,10 +27,6 @@ const WithRefs = () => {
 };
 
 describe('<Tabs />', () => {
-  it("true is true", () => {
-    expect(true).toBeTruthy()
-  });
-/*
   it('renders elements with correct tab related roles', () => {
     render(
       <Tabs>
@@ -215,5 +211,4 @@ describe('<Tabs />', () => {
     expect(tabPanel1).toHaveAttribute('aria-expanded', 'false');
     expect(tabPanel2).toHaveAttribute('aria-expanded', 'true');
   });
-  */
 });
