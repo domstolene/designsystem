a9840cb5d05aa16da83ffb3e645b9b43b0da00d1
Petter Sæther Moen <pettersmoen@gmail.com>
2023-11-09 11:10:46 +0100

    oppdater eslint config iht. anbefalinger for typescript-eslint v6

---
+++ .eslintrc.cjs
@@ -0,0 +1,33 @@
+module.exports = {
+  root: true,
+  parser: '@typescript-eslint/parser',
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/stylistic',
+  ],
+  env: {
+    es6: true,
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+  plugins: ['react', '@typescript-eslint'],
+  rules: {
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    '@typescript-eslint/no-empty-function': 'off',
+    '@typescript-eslint/no-unused-vars': 'off',
+  },
+  overrides: [
+    {
+      files: ['packages/**/*.ts', 'packages/**/*.tsx'],
+      parserOptions: {
+        project: true,
+        tsconfigRootDir: __dirname,
+      },
+    },
+  ],
+};
--- packages/components/src/components/Spinner/Spinner.tsx
+++ packages/components/src/components/Spinner/Spinner.tsx
@@ -64,7 +64,7 @@ export type SpinnerProps = BaseComponentProps<
   SVGElement,
   {
     /**Farge på spinneren. */
-    color?: TextColor | string;
+    color?: TextColor | (string & {});
     /**Størrelse; Setter høyde og bredde på spinneren. */
     size?: Property.Width<string>;
     /**Tekst som vises ved hover. */
