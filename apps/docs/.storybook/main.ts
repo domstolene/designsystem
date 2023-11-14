b16ca974c356366af5afca85b41febe6beec2566
Petter Sæther Moen <pettersmoen@gmail.com>
2023-11-14 13:34:57 +0100

    kun filtrer ut react-typer fra docgen

--- apps/docs/.storybook/main.ts
+++ apps/docs/.storybook/main.ts
@@ -1,27 +1,29 @@
-import type { StorybookConfig } from "@storybook/react-vite";
+import type { StorybookConfig } from '@storybook/react-vite';
 
 const config: StorybookConfig = {
   stories: [
-    "../stories/**/*.stories.@(js|jsx|ts|tsx)",
-    "../stories/**/*.@(md|mdx)",
-    "../../../packages/*/src/**/*.stories.@(js|jsx|ts|tsx)",
-    "../../../packages/*/src/**/*.mdx",
+    '../stories/**/*.stories.@(js|jsx|ts|tsx)',
+    '../stories/**/*.@(md|mdx)',
+    '../../../packages/*/src/**/*.stories.@(js|jsx|ts|tsx)',
+    '../../../packages/*/src/**/*.mdx',
   ],
-  staticDirs: ["./images", "../../../packages/components/dist/assets/fonts"],
-  addons: ["@storybook/addon-essentials"],
+  staticDirs: ['./images', '../../../packages/components/dist/assets/fonts'],
+  addons: ['@storybook/addon-essentials'],
   framework: {
-    name: "@storybook/react-vite",
+    name: '@storybook/react-vite',
     options: {},
   },
   typescript: {
-    reactDocgen: "react-docgen-typescript",
+    reactDocgen: 'react-docgen-typescript',
     reactDocgenTypescriptOptions: {
       shouldExtractLiteralValuesFromEnum: true,
-      propFilter: (prop) => {
+      propFilter: prop => {
         if (
           prop.declarations === undefined ||
           prop.declarations === null ||
-          prop.declarations.every((dec) => /node_modules/.test(dec.fileName))
+          prop.declarations.every(dec =>
+            /node_modules\/@types\/react(-dom)?/.test(dec.fileName),
+          )
         ) {
           return false;
         }
