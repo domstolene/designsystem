29c3e4201fbf0a0c34fcd08e4deda7f28cef6dcd
Petter Sæther Moen <pettersmoen@gmail.com>
2023-12-05 15:40:52 +0100

    ELSA1-85 legge til oversettelsesfunksjoner for datoer

--- packages/datepicker/src/index.ts
+++ packages/datepicker/src/index.ts
@@ -2,3 +2,7 @@ export { Calendar } from './Calendar/Calendar';
 export type { CalendarProps } from './Calendar/Calendar';
 export { DatePicker } from './DatePicker';
 export type { DatePickerProps } from './DatePicker';
+export {
+  dateValueToNativeDate,
+  nativeDateToDateValue,
+} from './utils/transform';
