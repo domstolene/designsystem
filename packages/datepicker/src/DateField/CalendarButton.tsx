89afe6d751987fa8f65955c3dfa87d8ff86138cc
Petter Sæther Moen <pettersmoen@gmail.com>
2023-12-15 11:05:29 +0100

    ELSA1-85 bruk @react-aria/button for kalenderknapp-implementasjon
    @react-aria/button sin knappimplementasjon spiller mer på lag med
    DatePicker fra @react-aria/datepicker.

---
+++ packages/datepicker/src/DateField/CalendarButton.tsx
@@ -0,0 +1,92 @@
+import {
+  normalizeButton,
+  focusVisible,
+  selection,
+  CalendarIcon,
+  Icon,
+} from '@norges-domstoler/dds-components';
+import { useButton, AriaButtonProps } from '@react-aria/button';
+import styled, { css } from 'styled-components';
+import type { DateFieldProps } from './DateField';
+import { datePickerTokens } from '../DatePicker.tokens';
+import { useRef } from 'react';
+
+const StyledButton = styled.button<{
+  $componentSize: DateFieldProps['componentSize'];
+  $isDisabled: DateFieldProps['disabled'];
+}>`
+  ${normalizeButton}
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  ${({ $componentSize = 'medium' }) => css`
+    height: ${datePickerTokens.calendarButton[$componentSize].size};
+    width: ${datePickerTokens.calendarButton[$componentSize].size};
+    margin: calc(0px - ${datePickerTokens.calendarButton[$componentSize].size})
+      0;
+  `}
+  margin-left: -1px; // To align with TextInputs icons
+  border: 0;
+  padding: 0;
+  border-radius: ${datePickerTokens.calendarButton.borderRadius};
+
+  background-color: ${datePickerTokens.calendarButton.background};
+  color: ${({ $isDisabled }) =>
+    $isDisabled
+      ? datePickerTokens.calendarButton.disabled.color
+      : datePickerTokens.calendarButton.color};
+
+  transition: 50ms;
+
+  &:hover:not(.disabled) {
+    background-color: ${datePickerTokens.calendarButton.hover.background};
+    color: ${datePickerTokens.calendarButton.hover.color};
+  }
+
+  &:focus-visible:not(.disabled) {
+    ${focusVisible}
+    outline-offset: 0;
+  }
+
+  &.disabled {
+    outline: none;
+  }
+
+  *::selection {
+    ${selection}
+  }
+`;
+
+interface CalendarButtonProps extends AriaButtonProps {
+  componentSize: DateFieldProps['componentSize'];
+}
+
+export function CalendarButton({
+  componentSize,
+  ...props
+}: CalendarButtonProps) {
+  const ref = useRef<HTMLButtonElement>(null);
+  const { buttonProps } = useButton(props, ref);
+  return (
+    <StyledButton
+      {...buttonProps}
+      ref={ref}
+      type="button"
+      $isDisabled={props.isDisabled}
+      $componentSize={componentSize}
+      onClick={e => {
+        if (!props.isDisabled) {
+          buttonProps.onClick?.(e);
+        }
+      }}
+      className={[props.isDisabled ? 'disabled' : false]
+        .filter(Boolean)
+        .join(' ')}
+    >
+      <Icon
+        icon={CalendarIcon}
+        iconSize={componentSize === 'medium' ? 'medium' : 'small'}
+      />
+    </StyledButton>
+  );
+}
--- packages/datepicker/src/DateField/DateField.tsx
+++ packages/datepicker/src/DateField/DateField.tsx
@@ -1,37 +1,38 @@
-import { ComponentProps, Ref, forwardRef, useRef } from 'react';
+import { forwardRef, useRef } from 'react';
 import {
   AriaDateFieldOptions,
   useDateField,
   useDatePicker,
 } from '@react-aria/datepicker';
 import { useDateFieldState } from '@react-stately/datepicker';
-import { DateValue, createCalendar } from '@internationalized/date';
-import styled, { css } from 'styled-components';
 import {
-  CalendarIcon,
-  Icon,
+  CalendarDate,
+  DateValue,
+  createCalendar,
+} from '@internationalized/date';
+import styled from 'styled-components';
+import {
   InputMessage,
   Label,
   cn,
-  normalizeButton,
-  selection,
   InputProps,
   StatefulInput,
   StyledInputProps,
-  focusVisible,
 } from '@norges-domstoler/dds-components';
 
 import { DateSegment } from './DateSegment';
 import { datePickerTokens } from '../DatePicker.tokens';
 import { locale } from '../constants';
+import { CalendarButton } from './CalendarButton';
 
-export type DateFieldProps<T extends DateValue> = AriaDateFieldOptions<T> & {
-  className?: string;
-  buttonProps?: ReturnType<typeof useDatePicker>['buttonProps'];
-} & Pick<
-    InputProps,
-    'componentSize' | 'errorMessage' | 'tip' | 'disabled' | 'style'
-  >;
+export type DateFieldProps<T extends DateValue = CalendarDate> =
+  AriaDateFieldOptions<T> & {
+    className?: string;
+    buttonProps?: ReturnType<typeof useDatePicker>['buttonProps'];
+  } & Pick<
+      InputProps,
+      'componentSize' | 'errorMessage' | 'tip' | 'disabled' | 'style'
+    >;
 
 const DateFieldContainer = styled.div`
   display: inline-flex;
@@ -58,144 +59,84 @@ const DateSegmentContainer = styled.div`
   flex-direction: row;
 `;
 
-const CalendarButton = styled.button<{
-  $componentSize: DateFieldProps<DateValue>['componentSize'];
-  $isDisabled: DateFieldProps<DateValue>['disabled'];
-}>`
-  ${normalizeButton}
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  ${({ $componentSize = 'medium' }) => css`
-    height: ${datePickerTokens.calendarButton[$componentSize].size};
-    width: ${datePickerTokens.calendarButton[$componentSize].size};
-    margin: calc(0px - ${datePickerTokens.calendarButton[$componentSize].size})
-      0;
-  `}
-  margin-left: -1px; // To align with TextInputs icons
-  border: 0;
-  padding: 0;
-  border-radius: ${datePickerTokens.calendarButton.borderRadius};
+export const DateField = forwardRef<HTMLDivElement, DateFieldProps>(
+  (
+    {
+      errorMessage,
+      tip,
+      componentSize = 'medium',
+      style,
+      buttonProps,
+      ...props
+    },
+    forwardedRef,
+  ) => {
+    const state = useDateFieldState({
+      ...props,
+      locale,
+      createCalendar,
+    });
 
-  background-color: ${datePickerTokens.calendarButton.background};
-  color: ${({ $isDisabled }) =>
-    $isDisabled
-      ? datePickerTokens.calendarButton.disabled.color
-      : datePickerTokens.calendarButton.color};
+    const ref = useRef<HTMLInputElement>(null);
+    const { labelProps, fieldProps } = useDateField(props, state, ref);
 
-  transition: 50ms;
+    const hasErrorMessage = !!errorMessage;
+    const hasTip = !!tip;
+    const hasLabel = props.label != null;
+    const hasMessage = hasErrorMessage || hasTip;
+    const disabled = props.isDisabled || !!fieldProps['aria-disabled'];
 
-  &:hover:not(.disabled) {
-    background-color: ${datePickerTokens.calendarButton.hover.background};
-    color: ${datePickerTokens.calendarButton.hover.color};
-  }
-
-  &:focus-visible:not(.disabled) {
-    ${focusVisible}
-    outline-offset: 0;
-  }
-
-  &.disabled {
-    outline: none;
-  }
-
-  *::selection {
-    ${selection}
-  }
-`;
-
-function _DateField<T extends DateValue>(
-  {
-    errorMessage,
-    tip,
-    componentSize = 'medium',
-    style,
-    buttonProps: { onPress, ...buttonProps } = {},
-    ...props
-  }: DateFieldProps<T>,
-  forwardedRef: Ref<HTMLDivElement> = { current: null },
-) {
-  const state = useDateFieldState({
-    ...props,
-    locale,
-    createCalendar,
-  });
-
-  const ref = useRef<HTMLInputElement>(null);
-  const { labelProps, fieldProps } = useDateField(props, state, ref);
-
-  const hasErrorMessage = !!errorMessage;
-  const hasTip = !!tip;
-  const hasLabel = props.label != null;
-  const hasMessage = hasErrorMessage || hasTip;
-  const disabled = props.isDisabled || !!fieldProps['aria-disabled'];
-
-  return (
-    <DateFieldContainer className={props.className} ref={forwardedRef}>
-      {hasLabel && (
-        <Label {...labelProps} showRequiredStyling={props.isRequired}>
-          {props.label}
-        </Label>
-      )}
-      <InputDiv
-        {...fieldProps}
-        disabled={disabled}
-        componentSize={componentSize}
-        ref={ref}
-        hasErrorMessage={hasErrorMessage}
-        className={cn(
-          fieldProps.className,
-          disabled && 'disabled',
-          props.isOpen && 'active',
-          props.isReadOnly && 'read-only',
+    return (
+      <DateFieldContainer className={props.className} ref={forwardedRef}>
+        {hasLabel && (
+          <Label {...labelProps} showRequiredStyling={props.isRequired}>
+            {props.label}
+          </Label>
         )}
-        style={{
-          ...(fieldProps.style ?? {}),
-          ...(style ?? {}),
-        }}
-      >
-        {!props.isReadOnly && (
-          <CalendarButton
-            {...buttonProps}
-            type="button"
-            $isDisabled={disabled}
-            $componentSize={componentSize}
-            onClick={e => {
-              if (!disabled) {
-                const onClick = onPress as ComponentProps<'button'>['onClick'];
-                onClick?.(e);
-              }
-            }}
-            className={[disabled ? 'disabled' : false]
-              .filter(Boolean)
-              .join(' ')}
-          >
-            <Icon
-              icon={CalendarIcon}
-              iconSize={componentSize === 'medium' ? 'medium' : 'small'}
-            />
-          </CalendarButton>
-        )}
-        <DateSegmentContainer>
-          {state.segments.map((segment, i) => (
-            <DateSegment
+        <InputDiv
+          {...fieldProps}
+          disabled={disabled}
+          componentSize={componentSize}
+          ref={ref}
+          hasErrorMessage={hasErrorMessage}
+          className={cn(
+            fieldProps.className,
+            disabled && 'disabled',
+            props.isOpen && 'active',
+            props.isReadOnly && 'read-only',
+          )}
+          style={{
+            ...(fieldProps.style ?? {}),
+            ...(style ?? {}),
+          }}
+        >
+          {!props.isReadOnly && (
+            <CalendarButton
               componentSize={componentSize}
-              key={i}
-              segment={segment}
-              state={state}
+              {...buttonProps}
+              isDisabled={disabled}
             />
-          ))}
-        </DateSegmentContainer>
-      </InputDiv>
-      {hasMessage && (
-        <InputMessage
-          messageType={hasErrorMessage ? 'error' : 'tip'}
-          message={errorMessage ?? tip ?? ''}
-        />
-      )}
-    </DateFieldContainer>
-  );
-}
+          )}
+          <DateSegmentContainer>
+            {state.segments.map((segment, i) => (
+              <DateSegment
+                componentSize={componentSize}
+                key={i}
+                segment={segment}
+                state={state}
+              />
+            ))}
+          </DateSegmentContainer>
+        </InputDiv>
+        {hasMessage && (
+          <InputMessage
+            messageType={hasErrorMessage ? 'error' : 'tip'}
+            message={errorMessage ?? tip ?? ''}
+          />
+        )}
+      </DateFieldContainer>
+    );
+  },
+);
 
-export const DateField = forwardRef(_DateField);
 DateField.displayName = 'DateField';
