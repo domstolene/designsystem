ece2a933859c792d355ea88a12ef92f112489f72
Petter Sæther Moen <pettersmoen@gmail.com>
2024-01-02 16:37:14 +0100

    ELSA1-120 legge til flere stories

--- packages/datepicker/src/TimePicker/TimePicker.stories.tsx
+++ packages/datepicker/src/TimePicker/TimePicker.stories.tsx
@@ -1,17 +1,10 @@
 import { TimePicker, TimePickerProps } from '.';
 import { StoryTemplate } from '@norges-domstoler/storybook-components';
-import { useRef, useState } from 'react';
-import {
-  Button,
-  CalendarIcon,
-  Datepicker,
-  HStack,
-  Label,
-  Modal,
-  TextInput,
-  VStack,
-} from '@norges-domstoler/dds-components';
 import type { Meta } from '@storybook/react';
+import { TimeValue } from '@react-types/datepicker';
+import { Time } from '@internationalized/date';
+import { Button } from '@norges-domstoler/dds-components';
+import { useState } from 'react';
 
 const meta: Meta<typeof TimePicker> = {
   title: 'ALPHA/dds-datepicker/TimePicker',
@@ -20,10 +13,58 @@ const meta: Meta<typeof TimePicker> = {
 
 export default meta;
 
-export const Default = (args: Partial<TimePickerProps>) => {
+export const Default = (args: Partial<TimePickerProps<TimeValue>>) => {
   return (
     <StoryTemplate title="TimePicker - default">
-      <TimePicker label="Dato" {...args} />
+      <TimePicker label="Tidspunkt" {...args} />
+    </StoryTemplate>
+  );
+};
+
+export const Required = (args: Partial<TimePickerProps<TimeValue>>) => {
+  return (
+    <StoryTemplate title="DatePicker - default">
+      <TimePicker label="Tidspunkt" {...args} isRequired />
+    </StoryTemplate>
+  );
+};
+
+export const Controlled = (args: Partial<TimePickerProps<TimeValue>>) => {
+  const [value, setValue] = useState<TimeValue | null>(new Time(12));
+
+  return (
+    <StoryTemplate title="TimePicker - controlled">
+      <TimePicker
+        label="Tidspunkt"
+        {...args}
+        value={value}
+        onChange={setValue}
+      />
+      <Button onClick={() => setValue(new Time(12))}>
+        Sett til klokken 12
+      </Button>
+    </StoryTemplate>
+  );
+};
+
+export const Error = (args: Partial<TimePickerProps<TimeValue>>) => {
+  return (
+    <StoryTemplate title="TimePicker - error">
+      <TimePicker
+        label="Tidspunkt"
+        {...args}
+        errorMessage="Her er noe veldig galt! 😨"
+      />
+    </StoryTemplate>
+  );
+};
+
+export const OverviewSizes = (args: Partial<TimePickerProps<TimeValue>>) => {
+  return (
+    <StoryTemplate title="TimePicker - overview sizes">
+      <TimePicker {...args} componentSize="medium" label="Medium" />
+      <TimePicker {...args} componentSize="small" label="Small" />
+      <TimePicker {...args} componentSize="tiny" label="Tiny" />
     </StoryTemplate>
   );
 };
