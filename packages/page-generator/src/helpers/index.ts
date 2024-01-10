16790e5f362bcd9c64854d6537dce5e5e04651f1
Petter Sæther Moen <pettersmoen@gmail.com>
2024-01-10 12:33:49 +0100

    ELSA1-289 fjerne pagegenerator

--- packages/page-generator/src/helpers/index.ts
+++
@@ -1,7 +0,0 @@
-export * from './addFieldToState';
-export * from './getButtonRow';
-export * from './getComponent';
-export * from './isFieldWithValidations';
-export * from './isMultiValue';
-export * from './isPageGeneratorRow';
-export * from './isSectionGeneratorRow';
