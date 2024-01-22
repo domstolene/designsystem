abfae6265eb13aae96861794c557a9f0819d4594
Petter Sæther Moen <pettersmoen@gmail.com>
2024-01-22 13:57:09 +0100

    ELSA1-236 legge til ikon på `<Tag>`

--- packages/components/src/components/Tag/Tag.stories.tsx
+++ packages/components/src/components/Tag/Tag.stories.tsx
@@ -1,5 +1,8 @@
 import { StoryTemplate } from '@norges-domstoler/storybook-components';
 
+import { HStack, VStack } from '../Stack';
+import { Heading } from '../Typography';
+
 import { Tag, type TagProps } from '.';
 
 export default {
@@ -7,21 +10,43 @@ export default {
   component: Tag,
   argTypes: {
     text: { control: 'text' },
+    withIcon: { control: 'boolean' },
   },
 };
-
-export const Overview = () => (
-  <StoryTemplate title="Tag - overview">
-    <Tag text="default" />
-    <Tag text="success" purpose="success" />
-    <Tag text="danger" purpose="danger" />
-    <Tag text="warning" purpose="warning" />
-    <Tag text="info" purpose="info" />
-  </StoryTemplate>
-);
 
 export const Default = (args: TagProps) => (
   <StoryTemplate title="Tag - default" display="block">
     <Tag text="default" {...args} />
+  </StoryTemplate>
+);
+
+export const Overview = (args: TagProps) => (
+  <StoryTemplate title="Tag - overview">
+    <HStack gap="x4">
+      <VStack align="flex-start" gap="x1">
+        <Tag {...args} text="default" />
+        <Tag {...args} text="success" purpose="success" />
+        <Tag {...args} text="danger" purpose="danger" />
+        <Tag {...args} text="warning" purpose="warning" />
+        <Tag {...args} text="info" purpose="info" />
+      </VStack>
+      <VStack align="flex-start" gap="x1">
+        <Tag {...args} withIcon text="default" />
+        <Tag {...args} withIcon text="success" purpose="success" />
+        <Tag {...args} withIcon text="danger" purpose="danger" />
+        <Tag {...args} withIcon text="warning" purpose="warning" />
+        <Tag {...args} withIcon text="info" purpose="info" />
+      </VStack>
+    </HStack>
+  </StoryTemplate>
+);
+
+export const WithIcon = (args: TagProps) => (
+  <StoryTemplate title="Tag - with icon">
+    <Tag {...args} withIcon text="default" />
+    <Tag {...args} withIcon text="success" purpose="success" />
+    <Tag {...args} withIcon text="danger" purpose="danger" />
+    <Tag {...args} withIcon text="warning" purpose="warning" />
+    <Tag {...args} withIcon text="info" purpose="info" />
   </StoryTemplate>
 );
--- packages/components/src/components/Tag/Tag.tsx
+++ packages/components/src/components/Tag/Tag.tsx
@@ -3,6 +3,7 @@ import styled, { css } from 'styled-components';
 
 import { tagTokens as tokens, typographyType } from './Tag.tokens';
 import { type BaseComponentProps, getBaseHTMLProps } from '../../types';
+import { Icon } from '../Icon';
 import {
   TextOverflowEllipsisInner,
   TextOverflowEllipsisWrapper,
@@ -13,19 +14,26 @@ const { wrapper } = tokens;
 
 interface WrapperProps {
   $purpose: TagPurpose;
+  $withIcon: boolean;
 }
 
 const Wrapper = styled(TextOverflowEllipsisWrapper)<WrapperProps>`
   display: inline-flex;
   align-items: center;
+  gap: ${tokens.wrapper.icon.gap};
   border: ${wrapper.border};
   border-radius: ${wrapper.borderRadius};
-  padding: ${wrapper.padding};
+  padding: ${({ $withIcon }) =>
+    $withIcon ? wrapper.icon.padding : wrapper.padding};
   ${getFontStyling(typographyType)}
   ${({ $purpose }) => css`
     background-color: ${wrapper.purpose[$purpose].backgroundColor};
     border-color: ${wrapper.purpose[$purpose].borderColor};
   `}
+`;
+
+const StyledIcon = styled(Icon)<Pick<WrapperProps, '$purpose'>>`
+  color: ${({ $purpose }) => wrapper.purpose[$purpose].icon?.color};
 `;
 
 export type TagPurpose = 'success' | 'info' | 'danger' | 'warning' | 'default';
@@ -37,6 +45,11 @@ export type TagProps = BaseComponentProps<
     text?: string;
     /**Formål med status eller kategorisering. Påvirker styling. */
     purpose?: TagPurpose;
+    /**
+     * Om `<Tag>` skal ha et ikon til venstre for teksten. Tags med `purpose="default"` har aldri ikon.
+     * @default false
+     */
+    withIcon?: boolean;
   }
 >;
 
@@ -47,15 +60,26 @@ export const Tag = forwardRef<HTMLSpanElement, TagProps>((props, ref) => {
     id,
     className,
     htmlProps,
+    withIcon,
     ...rest
   } = props;
+
+  const icon = tokens.wrapper.purpose[purpose].icon;
 
   return (
     <Wrapper
       {...getBaseHTMLProps(id, className, htmlProps, rest)}
       ref={ref}
       $purpose={purpose}
+      $withIcon={Boolean(withIcon && icon)}
     >
+      {withIcon && icon && (
+        <StyledIcon
+          $purpose={purpose}
+          icon={icon.icon}
+          iconSize={tokens.wrapper.icon.size}
+        />
+      )}
       <TextOverflowEllipsisInner>{text}</TextOverflowEllipsisInner>
     </Wrapper>
   );
