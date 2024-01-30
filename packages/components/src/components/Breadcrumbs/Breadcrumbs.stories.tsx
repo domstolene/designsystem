12f42d10b324a4540f47537412fbd63c00e2da7e
Petter Sæther Moen <pettersmoen@gmail.com>
2024-01-30 09:10:53 +0100

    ELSA1-308 skriv om Breadcrumbs stories til `StoryObj`-format

--- packages/components/src/components/Breadcrumbs/Breadcrumbs.stories.tsx
+++ packages/components/src/components/Breadcrumbs/Breadcrumbs.stories.tsx
@@ -1,20 +1,32 @@
 import { StoryTemplate } from '@norges-domstoler/storybook-components';
+import { type Meta, type StoryObj } from '@storybook/react';
 
-import { Breadcrumb, Breadcrumbs, type BreadcrumbsProps } from '.';
+import { Breadcrumb, Breadcrumbs } from '.';
 
 export default {
   title: 'dds-components/Breadcrumbs',
   component: Breadcrumbs,
-};
+  parameters: {
+    docs: {
+      story: { inline: true },
+      canvas: { sourceState: 'hidden' },
+    },
+  },
+} satisfies Meta<typeof Breadcrumbs>;
 
-export const Default = (args: BreadcrumbsProps) => {
-  return (
-    <StoryTemplate title="Breadcrumbs - default" gap="0">
-      <Breadcrumbs {...args}>
-        <Breadcrumb href="#">Side 1</Breadcrumb>
-        <Breadcrumb href="#">Side 2</Breadcrumb>
-        <Breadcrumb>Side 3</Breadcrumb>
-      </Breadcrumbs>
+type Story = StoryObj<typeof Breadcrumbs>;
+
+export const Default: Story = {
+  decorators: Story => (
+    <StoryTemplate title="Breadcrumbs - default">
+      <Story />
     </StoryTemplate>
-  );
+  ),
+  args: {
+    children: [
+      <Breadcrumb href="#">Side 1</Breadcrumb>,
+      <Breadcrumb href="#">Side 2</Breadcrumb>,
+      <Breadcrumb>Side 3</Breadcrumb>,
+    ],
+  },
 };
