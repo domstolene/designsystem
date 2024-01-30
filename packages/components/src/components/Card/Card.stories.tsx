a3574a83c839655e093578d94a6096afce71cf3e
Petter Sæther Moen <pettersmoen@gmail.com>
2024-01-30 10:37:48 +0100

    ELSA1-308 skriv om Card stories til `StoryObj`-format

--- packages/components/src/components/Card/Card.stories.tsx
+++ packages/components/src/components/Card/Card.stories.tsx
@@ -1,5 +1,6 @@
 import { ddsBaseTokens } from '@norges-domstoler/dds-design-tokens';
 import { StoryTemplate } from '@norges-domstoler/storybook-components';
+import { type StoryObj } from '@storybook/react';
 import { useState } from 'react';
 import styled from 'styled-components';
 
@@ -11,19 +12,20 @@ import {
 import { Divider } from '../Divider';
 import { Typography } from '../Typography';
 
-import {
-  Card,
-  CardAccordion,
-  CardAccordionBody,
-  CardAccordionHeader,
-  type CardProps,
-  type ExpandableCardProps,
-} from '.';
+import { Card, CardAccordion, CardAccordionBody, CardAccordionHeader } from '.';
 
 export default {
   title: 'dds-components/Card',
   component: Card,
+  parameters: {
+    docs: {
+      story: { inline: true },
+      canvas: { sourceState: 'hidden' },
+    },
+  },
 };
+
+type Story = StoryObj<typeof Card>;
 
 const body = (
   <Typography>
@@ -41,100 +43,113 @@ const ContentContainer = styled.div`
   padding: ${ddsBaseTokens.spacing.SizesDdsSpacingX075};
 `;
 
-export const Overview = () => {
-  return (
+export const Overview: Story = {
+  decorators: Story => (
     <StoryTemplate title="Card - overview" display="grid" $columnsAmount={4}>
-      <Card cardType="info">
-        <ContentContainer>
-          <Typography typographyType="headingSans03">Title</Typography>
-          {body}
-        </ContentContainer>
-      </Card>
-      <Card cardType="info" color="filledDark">
-        <ContentContainer>
-          <Typography color="onDark" typographyType="headingSans03">
-            Title
-          </Typography>
-          {bodyOnDark}
-        </ContentContainer>
-      </Card>
-      <Card cardType="info" color="strokeDark">
-        <ContentContainer>
-          <Typography typographyType="headingSans03">Title</Typography>
-          {body}
-        </ContentContainer>
-      </Card>
-      <Card cardType="info" color="strokeLight">
-        <ContentContainer>
-          <Typography typographyType="headingSans03">Title</Typography>
-          {body}
-        </ContentContainer>
-      </Card>
-      <Card cardType="navigation" href="#">
-        <ContentContainer>
-          <Typography typographyType="headingSans03">Title</Typography>
-          {body}
-        </ContentContainer>
-      </Card>
-      <Card color="filledDark" cardType="navigation" href="#">
-        <ContentContainer>
-          <Typography color="onDark" typographyType="headingSans03">
-            Title
-          </Typography>
-          {bodyOnDark}
-        </ContentContainer>
-      </Card>
-      <Card color="strokeDark" cardType="navigation" href="#">
-        <ContentContainer>
-          <Typography typographyType="headingSans03">Title</Typography>
-          {body}
-        </ContentContainer>
-      </Card>
-      <Card color="strokeLight" cardType="navigation" href="#">
-        <ContentContainer>
-          <Typography typographyType="headingSans03">Title</Typography>
-          {body}
-        </ContentContainer>
-      </Card>
-      <Card cardType="info" color="filledLight">
-        <CardAccordion>
-          <CardAccordionHeader> Title </CardAccordionHeader>
-          <CardAccordionBody>Content</CardAccordionBody>
-        </CardAccordion>
-      </Card>
-      <Card cardType="info" color="filledDark">
-        <CardAccordion>
-          <CardAccordionHeader> Title </CardAccordionHeader>
-          <CardAccordionBody>Content</CardAccordionBody>
-        </CardAccordion>
-      </Card>
-      <Card cardType="info" color="strokeDark">
-        <CardAccordion>
-          <CardAccordionHeader> Title </CardAccordionHeader>
-          <CardAccordionBody>Content</CardAccordionBody>
-        </CardAccordion>
-      </Card>
-      <Card cardType="info" color="strokeLight">
-        <CardAccordion>
-          <CardAccordionHeader> Title </CardAccordionHeader>
-          <CardAccordionBody>Content</CardAccordionBody>
-        </CardAccordion>
-      </Card>
+      <Story />
     </StoryTemplate>
-  );
+  ),
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  render: (args: any) => (
+    <>
+      <Card {...args} cardType="info">
+        <ContentContainer>
+          <Typography typographyType="headingSans03">Title</Typography>
+          {body}
+        </ContentContainer>
+      </Card>
+      <Card {...args} cardType="info" color="filledDark">
+        <ContentContainer>
+          <Typography color="onDark" typographyType="headingSans03">
+            Title
+          </Typography>
+          {bodyOnDark}
+        </ContentContainer>
+      </Card>
+      <Card {...args} cardType="info" color="strokeDark">
+        <ContentContainer>
+          <Typography typographyType="headingSans03">Title</Typography>
+          {body}
+        </ContentContainer>
+      </Card>
+      <Card {...args} cardType="info" color="strokeLight">
+        <ContentContainer>
+          <Typography typographyType="headingSans03">Title</Typography>
+          {body}
+        </ContentContainer>
+      </Card>
+      <Card {...args} cardType="navigation" href="#">
+        <ContentContainer>
+          <Typography typographyType="headingSans03">Title</Typography>
+          {body}
+        </ContentContainer>
+      </Card>
+      <Card {...args} color="filledDark" cardType="navigation" href="#">
+        <ContentContainer>
+          <Typography color="onDark" typographyType="headingSans03">
+            Title
+          </Typography>
+          {bodyOnDark}
+        </ContentContainer>
+      </Card>
+      <Card {...args} color="strokeDark" cardType="navigation" href="#">
+        <ContentContainer>
+          <Typography typographyType="headingSans03">Title</Typography>
+          {body}
+        </ContentContainer>
+      </Card>
+      <Card {...args} color="strokeLight" cardType="navigation" href="#">
+        <ContentContainer>
+          <Typography typographyType="headingSans03">Title</Typography>
+          {body}
+        </ContentContainer>
+      </Card>
+      <Card {...args} cardType="info" color="filledLight">
+        <CardAccordion>
+          <CardAccordionHeader> Title </CardAccordionHeader>
+          <CardAccordionBody>Content</CardAccordionBody>
+        </CardAccordion>
+      </Card>
+      <Card {...args} cardType="info" color="filledDark">
+        <CardAccordion>
+          <CardAccordionHeader> Title </CardAccordionHeader>
+          <CardAccordionBody>Content</CardAccordionBody>
+        </CardAccordion>
+      </Card>
+      <Card {...args} cardType="info" color="strokeDark">
+        <CardAccordion>
+          <CardAccordionHeader> Title </CardAccordionHeader>
+          <CardAccordionBody>Content</CardAccordionBody>
+        </CardAccordion>
+      </Card>
+      <Card {...args} cardType="info" color="strokeLight">
+        <CardAccordion>
+          <CardAccordionHeader> Title </CardAccordionHeader>
+          <CardAccordionBody>Content</CardAccordionBody>
+        </CardAccordion>
+      </Card>
+    </>
+  ),
 };
 
-export const Default = (args: CardProps) => {
-  return (
+export const Default: Story = {
+  args: { children: 'Content' },
+  decorators: Story => (
     <StoryTemplate title="Card - default">
-      <Card {...args}>Content</Card>
+      <Story />
     </StoryTemplate>
-  );
+  ),
 };
 
-export const Accordion = (args: ExpandableCardProps) => {
-  return (
+export const Accordion: Story = {
+  decorators: Story => (
     <StoryTemplate title="Card - accordion" gap="0">
+      <Story />
+    </StoryTemplate>
+  ),
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  render: (args: any) => (
+    <>
       <Card {...args} cardType="expandable">
         <CardAccordion>
           <CardAccordionHeader>Dekning av reiseutgifter</CardAccordionHeader>
@@ -195,15 +210,21 @@ export const Accordion = (args: ExpandableCardProps) => {
           <CardAccordionBody>Content</CardAccordionBody>
         </CardAccordion>
       </Card>
-    </StoryTemplate>
-  );
+    </>
+  ),
 };
 
-export const AccordionControlled = (args: ExpandableCardProps) => {
-  const [isExpanded, setIsExpanded] = useState(true);
-
-  return (
+export const AccordionControlled: Story = {
+  decorators: Story => (
     <StoryTemplate title="Card - accordion" gap="0">
+      <Story />
+    </StoryTemplate>
+  ),
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  render: (args: any) => {
+    const [isExpanded, setIsExpanded] = useState(true);
+
+    return (
       <Card {...args} cardType="expandable">
         <CardAccordion
           isExpanded={isExpanded}
@@ -213,65 +234,71 @@ export const AccordionControlled = (args: ExpandableCardProps) => {
           <CardAccordionBody>Content</CardAccordionBody>
         </CardAccordion>
       </Card>
-    </StoryTemplate>
-  );
+    );
+  },
 };
 
-export const AccordionCustom = (args: ExpandableCardProps) => {
-  return (
+export const AccordionCustom: Story = {
+  decorators: Story => (
     <StoryTemplate title="Card - accordion" gap="0">
-      <Card {...args} cardType="expandable" color="strokeLight">
-        <CardAccordion>
-          <CardAccordionHeader
-            typographyType="bodySans01"
-            padding="4px 12px"
-            bold
-          >
-            Dekning av reiseutgifter
-          </CardAccordionHeader>
-          <CardAccordionBody padding="16px 12px">
-            <Typography typographyType="bodySans01">
-              I sivile saker avtales dekning av utgifter med den part som
-              innkalte deg. I straffesaker har du krav på reise- og
-              kostgodtgjørelse (
-              <Typography typographyType="a" href="#">
-                særavtale om dekning av utgifter til reise og kost
-              </Typography>
-              ). Reisen skal foretas på raskeste og rimeligste måte for staten.
-              Offentlig transport må benyttes der det er tilgjengelig.
-              Godtgjørelse for bruk av egen bil godtas bare i den utstrekning
-              det er rimeligst for det offentlige, med mindre særlige grunner
-              tilsier at du må bruke bil.Reiseutgiftene må dokumenteres med
-              kvitteringer, unntatt for rimeligste offentlig transport, for
-              eksempel buss, tog og så videre. For reiser over 15 km og som
-              varer utover 6 timer, dekkes utgifter til måltider etter satsene i
-              særavtalen om dekning av utgifter til reise og kost. Dersom
-              enkeltmåltider er dekket av andre enn deg selv, må du registrere
-              måltidsfradrag.
-            </Typography>
-          </CardAccordionBody>
-        </CardAccordion>
-      </Card>
+      <Story />
     </StoryTemplate>
-  );
+  ),
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  render: (args: any) => (
+    <Card {...args} cardType="expandable" color="strokeLight">
+      <CardAccordion>
+        <CardAccordionHeader
+          typographyType="bodySans01"
+          padding="4px 12px"
+          bold
+        >
+          Dekning av reiseutgifter
+        </CardAccordionHeader>
+        <CardAccordionBody padding="16px 12px">
+          <Typography typographyType="bodySans01">
+            I sivile saker avtales dekning av utgifter med den part som innkalte
+            deg. I straffesaker har du krav på reise- og kostgodtgjørelse (
+            <Typography typographyType="a" href="#">
+              særavtale om dekning av utgifter til reise og kost
+            </Typography>
+            ). Reisen skal foretas på raskeste og rimeligste måte for staten.
+            Offentlig transport må benyttes der det er tilgjengelig.
+            Godtgjørelse for bruk av egen bil godtas bare i den utstrekning det
+            er rimeligst for det offentlige, med mindre særlige grunner tilsier
+            at du må bruke bil.Reiseutgiftene må dokumenteres med kvitteringer,
+            unntatt for rimeligste offentlig transport, for eksempel buss, tog
+            og så videre. For reiser over 15 km og som varer utover 6 timer,
+            dekkes utgifter til måltider etter satsene i særavtalen om dekning
+            av utgifter til reise og kost. Dersom enkeltmåltider er dekket av
+            andre enn deg selv, må du registrere måltidsfradrag.
+          </Typography>
+        </CardAccordionBody>
+      </CardAccordion>
+    </Card>
+  ),
 };
 
-export const Examples = (args: CardProps) => {
-  return (
+export const Examples: Story = {
+  decorators: Story => (
     <StoryTemplate title="Card - examples">
-      <Card {...args}>
-        <ContentContainer>
-          <DescriptionList>
-            <DescriptionListTerm>Title</DescriptionListTerm>
-            <DescriptionListDesc>Description</DescriptionListDesc>
-          </DescriptionList>
-          <Divider />
-          <DescriptionList>
-            <DescriptionListTerm>Title</DescriptionListTerm>
-            <DescriptionListDesc>Description</DescriptionListDesc>
-          </DescriptionList>
-        </ContentContainer>
-      </Card>
+      <Story />
     </StoryTemplate>
-  );
+  ),
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  render: (args: any) => (
+    <Card {...args}>
+      <ContentContainer>
+        <DescriptionList>
+          <DescriptionListTerm>Title</DescriptionListTerm>
+          <DescriptionListDesc>Description</DescriptionListDesc>
+        </DescriptionList>
+        <Divider />
+        <DescriptionList>
+          <DescriptionListTerm>Title</DescriptionListTerm>
+          <DescriptionListDesc>Description</DescriptionListDesc>
+        </DescriptionList>
+      </ContentContainer>
+    </Card>
+  ),
 };
