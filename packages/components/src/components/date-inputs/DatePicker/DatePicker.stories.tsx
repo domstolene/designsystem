a495756196db879f9d534563cbe295409c26d9a9
Petter Sæther Moen <pettersmoen@gmail.com>
2024-01-30 15:43:01 +0100

    ELSA1-308 endre DatePicker stories til `StoryObj`-format

--- packages/components/src/components/date-inputs/DatePicker/DatePicker.stories.tsx
+++ packages/components/src/components/date-inputs/DatePicker/DatePicker.stories.tsx
@@ -7,7 +7,7 @@ import {
   today,
 } from '@internationalized/date';
 import { StoryTemplate } from '@norges-domstoler/storybook-components';
-import type { Meta } from '@storybook/react';
+import type { Meta, StoryObj } from '@storybook/react';
 import { useRef, useState } from 'react';
 
 import { Button } from '../../Button';
@@ -20,11 +20,17 @@ import {
   nativeDateToCalendarDate,
 } from '../utils/transform';
 
-import { DatePicker, type DatePickerProps } from '.';
+import { DatePicker } from '.';
 
 const meta: Meta<typeof DatePicker> = {
   title: 'dds-components/DatePicker',
   component: DatePicker,
+  parameters: {
+    docs: {
+      story: { inline: true },
+      canvas: { sourceState: 'hidden' },
+    },
+  },
   argTypes: {
     label: {
       control: 'text',
@@ -46,168 +52,220 @@ const meta: Meta<typeof DatePicker> = {
 
 export default meta;
 
-export const Default = (args: Partial<DatePickerProps>) => {
-  return (
+type Story = StoryObj<typeof DatePicker>;
+
+export const Default: Story = {
+  args: { label: 'Dato' },
+  decorators: Story => (
     <StoryTemplate title="DatePicker - default">
-      <DatePicker label="Dato" {...args} />
+      <Story />
     </StoryTemplate>
-  );
+  ),
 };
 
-export const Required = (args: Partial<DatePickerProps>) => {
-  return (
-    <StoryTemplate title="DatePicker - default">
-      <DatePicker label="Dato" {...args} isRequired />
+export const Required: Story = {
+  args: { label: 'Dato', isRequired: true },
+  decorators: Story => (
+    <StoryTemplate title="DatePicker - required">
+      <Story />
     </StoryTemplate>
-  );
+  ),
 };
 
-export const Controlled = (args: Partial<DatePickerProps>) => {
-  const [value, setValue] = useState<CalendarDate | null>(
-    new CalendarDate(2023, 8, 28),
-  );
-
-  return (
+export const Controlled: Story = {
+  args: { label: 'Dato' },
+  decorators: Story => (
     <StoryTemplate title="DatePicker - controlled">
-      <DatePicker label="Dato" {...args} value={value} onChange={setValue} />
-      <Button onClick={() => setValue(today('Europe/Oslo'))}>Set today</Button>
+      <Story />
     </StoryTemplate>
-  );
+  ),
+  render: args => {
+    const [value, setValue] = useState<CalendarDate | null>(
+      new CalendarDate(2023, 8, 28),
+    );
+
+    return (
+      <>
+        <DatePicker label="Dato" {...args} value={value} onChange={setValue} />
+        <Button onClick={() => setValue(today('Europe/Oslo'))}>
+          Set today
+        </Button>
+      </>
+    );
+  },
 };
 
-export const WeekendsUnavailable = (args: Partial<DatePickerProps>) => {
-  return (
+export const WeekendsUnavailable: Story = {
+  args: { label: 'Dato', isDateUnavailable: date => isWeekend(date, 'no-NO') },
+  decorators: Story => (
     <StoryTemplate title="DatePicker - weekends unavailable">
-      <DatePicker
-        label="Dato"
-        {...args}
-        isDateUnavailable={date => isWeekend(date, 'no-NO')}
-      />
+      <Story />
     </StoryTemplate>
-  );
+  ),
 };
 
-export const Error = (args: Partial<DatePickerProps>) => {
-  return (
+export const Error: Story = {
+  args: { label: 'Dato', errorMessage: 'Her er noe veldig galt! 😨' },
+  decorators: Story => (
     <StoryTemplate title="DatePicker - error">
-      <DatePicker
-        label="Dato"
-        {...args}
-        errorMessage="Her er noe veldig galt! 😨"
-      />
+      <Story />
     </StoryTemplate>
-  );
+  ),
 };
 
-export const OverviewSizes = (args: Partial<DatePickerProps>) => {
-  return (
-    <StoryTemplate title="DatePicker - overview sizes">
+export const OverviewSizes: Story = {
+  decorators: Story => (
+    <StoryTemplate title="DatePicker - weekends unavailable">
+      <Story />
+    </StoryTemplate>
+  ),
+  render: args => (
+    <>
       <DatePicker {...args} componentSize="medium" label="Medium" />
       <DatePicker {...args} componentSize="small" label="Small" />
       <DatePicker {...args} componentSize="tiny" label="Tiny" />
-    </StoryTemplate>
-  );
+    </>
+  ),
 };
 
-export const Tip = (args: Partial<DatePickerProps>) => {
-  return (
+export const Tip: Story = {
+  args: {
+    label: 'Dato',
+    tip: 'Visste du at denne komponenten også kan ha en tip?',
+  },
+  decorators: Story => (
     <StoryTemplate title="DatePicker - tip">
-      <DatePicker
-        label="Dato"
-        {...args}
-        tip="Visste du at denne komponenten også kan ha en tip?"
-      />
+      <Story />
     </StoryTemplate>
-  );
+  ),
 };
 
-export const ReadOnly = (args: Partial<DatePickerProps>) => {
-  return (
+export const ReadOnly: Story = {
+  args: {
+    label: 'Dato',
+    isReadOnly: true,
+    value: new CalendarDate(2023, 12, 24),
+  },
+  decorators: Story => (
     <StoryTemplate title="DatePicker - read only">
-      <DatePicker
-        label="Dato"
-        {...args}
-        value={new CalendarDate(2023, 12, 24)}
-        isReadOnly
-      />
+      <Story />
     </StoryTemplate>
-  );
+  ),
 };
 
-export const Disabled = (args: Partial<DatePickerProps>) => {
-  return (
+export const Disabled: Story = {
+  args: {
+    label: 'Dato',
+    isDisabled: true,
+    value: new CalendarDate(2023, 12, 24),
+  },
+  decorators: Story => (
     <StoryTemplate title="DatePicker - disabled">
-      <DatePicker label="Dato" {...args} isDisabled />
+      <Story />
     </StoryTemplate>
-  );
+  ),
 };
 
-export const CustomWidth = (args: Partial<DatePickerProps>) => {
-  return (
+export const CustomWidth: Story = {
+  args: { label: 'Dato', width: '1337px' },
+  decorators: Story => (
     <StoryTemplate title="DatePicker - custom width">
-      <DatePicker label="Dato" {...args} width="1337px" />
+      <Story />
     </StoryTemplate>
-  );
+  ),
 };
 
-export const ControlFocus = (args: Partial<DatePickerProps>) => {
-  const ref = useRef<HTMLElement>(null);
-  return (
-    <VStack gap="x1">
-      <DatePicker label="Dato" {...args} ref={ref} />
-      <Button onClick={() => ref.current?.focus()}>Fokuser DatePicker</Button>
-    </VStack>
-  );
-};
-
-export const InsideModal = (args: Partial<DatePickerProps>) => {
-  const [isOpen, setOpen] = useState(true);
-  return (
-    <StoryTemplate title="DatePicker - modal">
-      <Button onClick={() => setOpen(true)}>Åpne modal</Button>
-      <Modal isOpen={isOpen} onClose={() => setOpen(false)}>
-        <DatePicker label="Dato" {...args} />
-      </Modal>
+export const ControlFocus: Story = {
+  args: { label: 'Dato' },
+  decorators: Story => (
+    <StoryTemplate title="DatePicker - control focus">
+      <Story />
     </StoryTemplate>
-  );
+  ),
+  render: args => {
+    const ref = useRef<HTMLElement>(null);
+    return (
+      <VStack gap="x1">
+        <DatePicker label="Dato" {...args} ref={ref} />
+        <Button onClick={() => ref.current?.focus()}>Fokuser DatePicker</Button>
+      </VStack>
+    );
+  },
 };
 
-export const DateAndTime = () => {
-  const norwegianDateFormatter = new DateFormatter('no-NO', {
-    dateStyle: 'full',
-    timeStyle: 'short',
-  });
-  const [date, setDate] = useState<CalendarDate>(today('Europe/Oslo'));
-  const [time, setTime] = useState<Time>(new Time(12, 0, 0));
-  const dateTime = toCalendarDateTime(date, time);
-  return (
+export const InsideModal: Story = {
+  args: { label: 'Dato' },
+  decorators: Story => (
+    <StoryTemplate title="DatePicker - inside modal">
+      <Story />
+    </StoryTemplate>
+  ),
+  render: args => {
+    const [isOpen, setOpen] = useState(true);
+    return (
+      <>
+        <Button onClick={() => setOpen(true)}>Åpne modal</Button>
+        <Modal isOpen={isOpen} onClose={() => setOpen(false)}>
+          <DatePicker label="Dato" {...args} />
+        </Modal>
+      </>
+    );
+  },
+};
+
+export const DateAndTime: Story = {
+  args: { label: 'Dato' },
+  decorators: Story => (
     <StoryTemplate title="DatePicker - date and time">
-      <HStack gap="x1">
-        <DatePicker
-          label="Dato"
-          value={date}
-          onChange={newValue => setDate(newValue)}
-        />
-        <TimePicker
-          label="Tid"
-          value={time}
-          onChange={newTime => setTime(newTime)}
-        />
-      </HStack>
-      <Paragraph>
-        Valg dato og tid:{' '}
-        {norwegianDateFormatter.format(dateTime.toDate('Europe/Oslo'))}
-      </Paragraph>
+      <Story />
     </StoryTemplate>
-  );
+  ),
+  render: args => {
+    const norwegianDateFormatter = new DateFormatter('no-NO', {
+      dateStyle: 'full',
+      timeStyle: 'short',
+    });
+    const [date, setDate] = useState<CalendarDate>(today('Europe/Oslo'));
+    const [time, setTime] = useState<Time>(new Time(12, 0, 0));
+    const dateTime = toCalendarDateTime(date, time);
+    return (
+      <VStack gap="x1">
+        <HStack gap="x1">
+          <DatePicker
+            {...args}
+            value={date}
+            onChange={newValue => setDate(newValue)}
+          />
+          <TimePicker
+            label="Tid"
+            value={time}
+            onChange={newTime => setTime(newTime)}
+          />
+        </HStack>
+        <Paragraph>
+          Valg dato og tid:{' '}
+          {norwegianDateFormatter.format(dateTime.toDate('Europe/Oslo'))}
+        </Paragraph>
+      </VStack>
+    );
+  },
 };
 
-export const NativeDate = () => {
-  const [date, setDate] = useState<Date>(new Date());
-  return (
-    <DatePicker
-      value={nativeDateToCalendarDate(date)}
-      onChange={d => setDate(calendarDateToNativeDate(d))}
-    />
-  );
+export const NativeDate: Story = {
+  args: { label: 'Dato' },
+  decorators: Story => (
+    <StoryTemplate title="DatePicker - native date">
+      <Story />
+    </StoryTemplate>
+  ),
+  render: args => {
+    const [date, setDate] = useState<Date>(new Date());
+    return (
+      <DatePicker
+        {...args}
+        value={nativeDateToCalendarDate(date)}
+        onChange={d => setDate(calendarDateToNativeDate(d))}
+      />
+    );
+  },
 };
