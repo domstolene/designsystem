65fb3a24fc05ee9784d367b23a1faf6d27a5f564
Petter Sæther Moen <pettersmoen@gmail.com>
2024-01-30 16:07:14 +0100

    ELSA1-308 endre Divider stories til `StoryObj`-format

--- packages/components/src/components/Divider/Divider.stories.tsx
+++ packages/components/src/components/Divider/Divider.stories.tsx
@@ -1,56 +1,67 @@
 import { StoryTemplate } from '@norges-domstoler/storybook-components';
+import { type StoryObj } from '@storybook/react';
 
 import { Typography } from '../Typography';
 
-import { Divider, type DividerProps } from '.';
+import { Divider } from '.';
 
 export default {
   title: 'dds-components/Divider',
   component: Divider,
-  argTypes: {},
+  parameters: {
+    docs: {
+      story: { inline: true },
+      canvas: { sourceState: 'hidden' },
+    },
+  },
 };
 
-export const Overview = (args: DividerProps) => {
-  return (
+type Story = StoryObj<typeof Divider>;
+
+export const Overview: Story = {
+  decorators: Story => (
     <StoryTemplate title="Divider - overview" display="block">
-      <div>
-        <Divider {...args} />
-        <Divider {...args} color="primaryLighter" />
-      </div>
+      <Story />
     </StoryTemplate>
-  );
+  ),
+  render: args => (
+    <div>
+      <Divider {...args} />
+      <Divider {...args} color="primaryLighter" />
+    </div>
+  ),
 };
 
-export const Default = (args: DividerProps) => {
-  return (
+export const Default: Story = {
+  decorators: Story => (
     <StoryTemplate title="Divider - default" display="block">
-      <div>
-        <Divider {...args} />
-      </div>
+      <Story />
     </StoryTemplate>
-  );
+  ),
 };
 
-export const Example = (args: DividerProps) => {
-  return (
+export const Example: Story = {
+  decorators: Story => (
     <StoryTemplate title="Divider - example">
-      <div>
-        <Typography withMargins>
-          Vitner er svært viktige for rettssikkerheten i samfunnet. Du har fått
-          en innkalling (kalles stevning) til saken fordi partene eller retten
-          mener du kjenner til noe som kan være viktig for retten å vite. Du har
-          plikt til å møte opp i tråd med innkallingen og forklare deg for
-          retten.
-        </Typography>
-        <Divider {...args} />
-        <Typography withMargins>
-          En rettssak avgjøres med bakgrunn i det som er kommet fram i
-          rettssalen. Derfor må vitner forklare seg for retten slik at dommerne
-          hører vitneforklaringen. Det gjelder selv om man tidligere ha forklart
-          seg for politiet eller partenes advokat. En vitneforklaring kan
-          avgjøre utfallet av en sak, derfor har vitner plikt til å snakke sant.
-        </Typography>
-      </div>
+      <Story />
     </StoryTemplate>
-  );
+  ),
+  render: args => (
+    <div>
+      <Typography withMargins>
+        Vitner er svært viktige for rettssikkerheten i samfunnet. Du har fått en
+        innkalling (kalles stevning) til saken fordi partene eller retten mener
+        du kjenner til noe som kan være viktig for retten å vite. Du har plikt
+        til å møte opp i tråd med innkallingen og forklare deg for retten.
+      </Typography>
+      <Divider {...args} />
+      <Typography withMargins>
+        En rettssak avgjøres med bakgrunn i det som er kommet fram i rettssalen.
+        Derfor må vitner forklare seg for retten slik at dommerne hører
+        vitneforklaringen. Det gjelder selv om man tidligere ha forklart seg for
+        politiet eller partenes advokat. En vitneforklaring kan avgjøre utfallet
+        av en sak, derfor har vitner plikt til å snakke sant.
+      </Typography>
+    </div>
+  ),
 };
