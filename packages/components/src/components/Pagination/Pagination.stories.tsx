26a3447d5a19f78773bdbd8a5120afa88f3c0091
Petter Sæther Moen <pettersmoen@gmail.com>
2024-01-31 11:48:07 +0100

    ELSA1-308 endre Pagination stories til `StoryObj`-format

--- packages/components/src/components/Pagination/Pagination.stories.tsx
+++ packages/components/src/components/Pagination/Pagination.stories.tsx
@@ -1,6 +1,9 @@
 import { StoryTemplate } from '@norges-domstoler/storybook-components';
+import { type StoryObj } from '@storybook/react';
 
-import { Pagination, type PaginationProps } from '.';
+import { VStack } from '../Stack';
+
+import { Pagination } from '.';
 
 export default {
   title: 'dds-components/Pagination',
@@ -14,8 +17,9 @@ export default {
     defaultActivePage: { control: 'number' },
   },
   parameters: {
-    controls: {
-      exclude: ['onChange', 'selectOptions'],
+    docs: {
+      story: { inline: true },
+      canvas: { sourceState: 'hidden' },
     },
   },
 };
@@ -26,70 +30,87 @@ const customOptions = [17, 32, customOptionsItemsAmount].map(o => ({
   value: o,
 }));
 
-export const Overview = (args: PaginationProps) => (
-  <StoryTemplate
-    title="Pagination - overview"
-    gap="50px"
-    containerStyle={{ alignItems: 'stretch' }}
-  >
-    <Pagination {...args} itemsAmount={70} />
-    <Pagination {...args} itemsAmount={100} />
-    <Pagination {...args} defaultActivePage={6} itemsAmount={100} />
-    <Pagination {...args} withCounter itemsAmount={100} />
-    <Pagination
-      {...args}
-      withCounter
-      defaultItemsPerPage={20}
-      itemsAmount={100}
-    />
-    <Pagination
-      {...args}
-      withPagination={false}
-      withCounter
-      itemsAmount={100}
-    />
-    <Pagination {...args} withSelect itemsAmount={100} />
-    <Pagination {...args} withCounter withSelect itemsAmount={100} />
-    <Pagination
-      {...args}
-      withCounter
-      withSelect
-      withPagination={false}
-      itemsAmount={100}
-    />
-    <Pagination
-      {...args}
-      withCounter
-      withSelect
-      defaultItemsPerPage={customOptions[0].value}
-      selectOptions={customOptions}
-      itemsAmount={customOptionsItemsAmount}
-    />
-  </StoryTemplate>
-);
+type Story = StoryObj<typeof Pagination>;
 
-export const OverviewMobile = (args: PaginationProps) => (
-  <StoryTemplate title="Pagination - mobile overview" gap="50px">
-    <Pagination {...args} smallScreen itemsAmount={100} />
-    <Pagination {...args} smallScreen withCounter itemsAmount={100} />
-    <Pagination
-      {...args}
-      smallScreen
-      withSelect
-      itemsAmount={args.itemsAmount || 100}
-    />
-    <Pagination
-      {...args}
-      smallScreen
-      withCounter
-      withSelect
-      itemsAmount={args.itemsAmount || 100}
-    />
-  </StoryTemplate>
-);
+export const Default: Story = {
+  args: { itemsAmount: 100 },
+  decorators: Story => (
+    <StoryTemplate title="Pagination - default">
+      <Story />
+    </StoryTemplate>
+  ),
+};
 
-export const Default = (args: PaginationProps) => (
-  <StoryTemplate title="Pagination - default" display="block">
-    <Pagination {...args} itemsAmount={args.itemsAmount || 100} />
-  </StoryTemplate>
-);
+export const Overview: Story = {
+  args: {},
+  decorators: Story => (
+    <StoryTemplate title="Pagination - overview">
+      <Story />
+    </StoryTemplate>
+  ),
+  render: args => (
+    <VStack gap="x2">
+      <Pagination {...args} itemsAmount={70} />
+      <Pagination {...args} itemsAmount={100} />
+      <Pagination {...args} defaultActivePage={6} itemsAmount={100} />
+      <Pagination {...args} withCounter itemsAmount={100} />
+      <Pagination
+        {...args}
+        withCounter
+        defaultItemsPerPage={20}
+        itemsAmount={100}
+      />
+      <Pagination
+        {...args}
+        withPagination={false}
+        withCounter
+        itemsAmount={100}
+      />
+      <Pagination {...args} withSelect itemsAmount={100} />
+      <Pagination {...args} withCounter withSelect itemsAmount={100} />
+      <Pagination
+        {...args}
+        withCounter
+        withSelect
+        withPagination={false}
+        itemsAmount={100}
+      />
+      <Pagination
+        {...args}
+        withCounter
+        withSelect
+        defaultItemsPerPage={customOptions[0].value}
+        selectOptions={customOptions}
+        itemsAmount={customOptionsItemsAmount}
+      />
+    </VStack>
+  ),
+};
+
+export const OverviewMobile: Story = {
+  args: {},
+  decorators: Story => (
+    <StoryTemplate title="Pagination - overview mobile">
+      <Story />
+    </StoryTemplate>
+  ),
+  render: args => (
+    <VStack gap="x2">
+      <Pagination {...args} smallScreen itemsAmount={100} />
+      <Pagination {...args} smallScreen withCounter itemsAmount={100} />
+      <Pagination
+        {...args}
+        smallScreen
+        withSelect
+        itemsAmount={args.itemsAmount || 100}
+      />
+      <Pagination
+        {...args}
+        smallScreen
+        withCounter
+        withSelect
+        itemsAmount={args.itemsAmount || 100}
+      />
+    </VStack>
+  ),
+};
