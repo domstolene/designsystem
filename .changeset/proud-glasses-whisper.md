ade4158e295fa2d84b1d36edc3f901d86d367513
AgataJedryszek <agata.jedryszek@bouvet.no>
2024-03-04 10:13:03 +0100

    ELSA1-330 Justerer spacing i `<Chip />`

---
+++ .changeset/proud-glasses-whisper.md
@@ -0,0 +1,5 @@
+---
+"@norges-domstoler/dds-components": patch
+---
+
+Justerer spacing i `<Chip />`.
--- packages/components/src/components/Chip/Chip.tokens.tsx
+++ packages/components/src/components/Chip/Chip.tokens.tsx
@@ -8,7 +8,12 @@ export const typographyType: StaticTypographyType = 'bodySans01';
 
 const container = {
   gap: spacing.SizesDdsSpacingX025,
-  padding: `${spacing.SizesDdsSpacingX0125} ${spacing.SizesDdsSpacingX025}`,
+  padding: [
+    spacing.SizesDdsSpacingX0125,
+    spacing.SizesDdsSpacingX025,
+    spacing.SizesDdsSpacingX0125,
+    spacing.SizesDdsSpacingX05,
+  ].join(' '),
   backgroundColor: colors.DdsColorNeutralsGray1,
   border: `1px solid ${colors.DdsColorNeutralsGray3}`,
   borderRadius: borderRadius.RadiiDdsBorderRadius1Radius,
