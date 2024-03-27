a19ccc5ebe42d362c22ce7ce122c656febb48cb3
Agata Jedryszek <agata.jedryszek@bouvet.no>
2024-03-27 11:00:43 +0100

    ELSA1-359 Tester for DatePicker og TimePicker

--- packages/components/src/components/date-inputs/DatePicker/DatePicker.spec.tsx
+++ packages/components/src/components/date-inputs/DatePicker/DatePicker.spec.tsx
@@ -5,7 +5,7 @@ import { DatePicker } from './DatePicker';
 import { HStack } from '../../Stack';
 import { TextInput } from '../../TextInput';
 
-describe('<DatePicker>', () => {
+describe('<DatePicker />', () => {
   it('is same height as <TextInput>', () => {
     render(
       <HStack>
@@ -21,5 +21,38 @@ describe('<DatePicker>', () => {
     const textInputHeight = textInput.getBoundingClientRect().height;
 
     expect(datePickerHeight).toBe(textInputHeight);
+  });
+  it('should render calender button', () => {
+    render(<DatePicker />);
+
+    const button = screen.queryByRole('button');
+    expect(button).toBeInTheDocument();
+  });
+  it('calender button should have aria attributes', () => {
+    render(<DatePicker />);
+
+    const button = screen.getByRole('button');
+    expect(button).toHaveAttribute('aria-haspopup', 'dialog');
+  });
+  it('should render three spinbuttons for the date', () => {
+    render(<DatePicker />);
+
+    const spinbuttons = screen.getAllByRole('spinbutton');
+    expect(spinbuttons).toHaveLength(3);
+  });
+  it('spinbuttons should have aria-valuemin', () => {
+    render(<DatePicker />);
+
+    const spinbuttons = screen.getAllByRole('spinbutton');
+    expect(spinbuttons[0]).toHaveAttribute('aria-valuemin', '1');
+    expect(spinbuttons[1]).toHaveAttribute('aria-valuemin', '1');
+    expect(spinbuttons[2]).toHaveAttribute('aria-valuemin', '1');
+  });
+  it('spinbuttons should have aria-valuemax', () => {
+    render(<DatePicker />);
+
+    const spinbuttons = screen.getAllByRole('spinbutton');
+    expect(spinbuttons[0]).toHaveAttribute('aria-valuemax', '31');
+    expect(spinbuttons[1]).toHaveAttribute('aria-valuemax', '12');
   });
 });
---
+++ packages/components/src/components/date-inputs/TimePicker/TimePicker.spec.tsx
@@ -0,0 +1,45 @@
+import { render, screen } from '@testing-library/react';
+import { describe, expect, it } from 'vitest';
+
+import { TimePicker } from './TimePicker';
+import { HStack } from '../../Stack';
+import { TextInput } from '../../TextInput';
+
+describe('<TimePicker />', () => {
+  it('is same height as <TextInput>', () => {
+    render(
+      <HStack>
+        <TimePicker label="Dato" data-testid="timepicker" />
+        <TextInput label="Dato" data-testid="textinput" />
+      </HStack>,
+    );
+
+    const timePicker = screen.getByTestId('timepicker');
+    const textInput = screen.getByTestId('textinput');
+
+    const timePickerHeight = timePicker.getBoundingClientRect().height;
+    const textInputHeight = textInput.getBoundingClientRect().height;
+
+    expect(timePickerHeight).toBe(textInputHeight);
+  });
+  it('should render three spinbuttons for the time', () => {
+    render(<TimePicker />);
+
+    const spinbuttons = screen.getAllByRole('spinbutton');
+    expect(spinbuttons).toHaveLength(2);
+  });
+  it('spinbuttons should have aria-valuemin', () => {
+    render(<TimePicker />);
+
+    const spinbuttons = screen.getAllByRole('spinbutton');
+    expect(spinbuttons[0]).toHaveAttribute('aria-valuemin', '0');
+    expect(spinbuttons[1]).toHaveAttribute('aria-valuemin', '0');
+  });
+  it('spinbuttons should have aria-valuemax', () => {
+    render(<TimePicker />);
+
+    const spinbuttons = screen.getAllByRole('spinbutton');
+    expect(spinbuttons[0]).toHaveAttribute('aria-valuemax', '23');
+    expect(spinbuttons[1]).toHaveAttribute('aria-valuemax', '59');
+  });
+});
