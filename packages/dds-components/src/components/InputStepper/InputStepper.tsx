5787c8a230969f4d9d44b59d5aa18be59850b1a1
Espen Bjørnsen <espen.bjornsen@bouvet.no>
2025-03-03 15:10:12 +0100

    ELSA1-565 fix merge conflict

--- packages/dds-components/src/components/InputStepper/InputStepper.tsx
+++ packages/dds-components/src/components/InputStepper/InputStepper.tsx
@@ -81,77 +81,68 @@ export const InputStepper = ({
     }
   };
 
-    const styleVariables: Properties = {
-      // eslint-disable-next-line @typescript-eslint/no-explicit-any
-      ['--dds-input-stepper-width' as any]: width ? width : '85px',
-    };
-    return (
-      <div className={cn(className)}>
-        <div>
-          <Label
-            htmlFor={uniqueId}
-            readOnly={readOnly}
-            className={cn(inputStyles.label)}
-          >
-            {label}
-          </Label>
-        </div>
-        <div className={cn(styles['input-container'])}>
-          {readOnly || disabled ? null : (
-            <Button
-              aria-label={decreaseButtonLabel ?? `Trekk fra ${label}`}
-              purpose="secondary"
-              size={componentSize}
-              icon={MinusIcon}
-              onClick={handleMinus}
-              className={cn(styles['stepButton--left'], styles['stepButton'])}
-              aria-controls={uniqueId}
-            ></Button>
-          )}
-          <StatefulInput
-            type="text"
-            disabled={disabled}
-            readOnly={readOnly}
-            inputMode="numeric"
-            pattern="-?[0-9]+"
-            id={uniqueId}
-            ref={ref}
-            style={{ ...style, ...styleVariables }}
-            componentSize={componentSize}
-            {...getBaseHTMLProps(
-              uniqueId,
-              cn(styles.textInput),
-              htmlProps,
-              rest,
-            )}
-            value={inputValue}
-            onChange={handleInput}
-            hasErrorMessage={hasErrorMessage}
-            aria-invalid={hasErrorMessage ? true : undefined}
-            aria-describedby={spaceSeparatedIdListGenerator([
-              hasErrorMessage ? errorMessageId : undefined,
-              hasTip ? tipId : undefined,
-            ])}
-          />
-          {readOnly || disabled ? null : (
-            <Button
-              aria-label={increaseButtonLabel ?? `Legg til ${label}`}
-              purpose="secondary"
-              size={componentSize}
-              icon={PlusIcon}
-              onClick={handlePlus}
-              className={cn(styles['stepButton--right'], styles['stepButton'])}
-              aria-controls={uniqueId}
-            ></Button>
-          )}
-        </div>
-        {hasMessage &&
-          renderInputMessage(tip, tipId, errorMessage, errorMessageId)}
+  const styleVariables: Properties = {
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    ['--dds-input-stepper-width' as any]: width ? width : '85px',
+  };
+  return (
+    <div className={cn(className)}>
+      <div>
+        <Label
+          htmlFor={uniqueId}
+          readOnly={readOnly}
+          className={cn(inputStyles.label)}
+        >
+          {label}
+        </Label>
+      </div>
+      <div className={cn(styles['input-container'])}>
+        {readOnly || disabled ? null : (
+          <Button
+            aria-label={decreaseButtonLabel ?? `Trekk fra ${label}`}
+            purpose="secondary"
+            size={componentSize}
+            icon={MinusIcon}
+            onClick={handleMinus}
+            className={cn(styles['stepButton--left'], styles['stepButton'])}
+            aria-controls={uniqueId}
+          ></Button>
+        )}
+        <StatefulInput
+          type="text"
+          disabled={disabled}
+          readOnly={readOnly}
+          inputMode="numeric"
+          pattern="-?[0-9]+"
+          id={uniqueId}
+          style={{ ...style, ...styleVariables }}
+          componentSize={componentSize}
+          {...getBaseHTMLProps(uniqueId, cn(styles.textInput), htmlProps, rest)}
+          value={inputValue}
+          onChange={handleInput}
+          hasErrorMessage={hasErrorMessage}
+          aria-invalid={hasErrorMessage ? true : undefined}
+          aria-describedby={spaceSeparatedIdListGenerator([
+            hasErrorMessage ? errorMessageId : undefined,
+            hasTip ? tipId : undefined,
+          ])}
+        />
+        {readOnly || disabled ? null : (
+          <Button
+            aria-label={increaseButtonLabel ?? `Legg til ${label}`}
+            purpose="secondary"
+            size={componentSize}
+            icon={PlusIcon}
+            onClick={handlePlus}
+            className={cn(styles['stepButton--right'], styles['stepButton'])}
+            aria-controls={uniqueId}
+          ></Button>
+        )}
       </div>
       {hasMessage &&
         renderInputMessage(tip, tipId, errorMessage, errorMessageId)}
+      ;
     </div>
   );
 };
-
 InputStepper.displayName = 'InputStepper';
