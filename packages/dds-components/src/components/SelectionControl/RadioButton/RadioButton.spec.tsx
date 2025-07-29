6f36b21acea4581d27f42f20fa204528634fea05
Agata Jedryszek <agata.jedryszek@domstol.no>
2025-07-29 13:12:15 +0200

    ELSA1-622 Flere tester for selection controls
    Lager flere tester for `<Checkbox>`, `<RadioButton>` og deres grupper.

--- packages/dds-components/src/components/SelectionControl/RadioButton/RadioButton.spec.tsx
+++ packages/dds-components/src/components/SelectionControl/RadioButton/RadioButton.spec.tsx
@@ -43,8 +43,33 @@ describe('<RadioButton>', () => {
     const label = 'Select me';
     render(<RadioButton label={label} />);
     expect(screen.getByText(label)).toBeInTheDocument();
+    expect(screen.getByRole('radio')).toHaveAccessibleName(label);
   });
   describe('<RadioButtonGroup>', () => {
+    it('should have accessible name', () => {
+      const label = 'label';
+      render(
+        <RadioButtonGroup label={label}>
+          <RadioButton />
+        </RadioButtonGroup>,
+      );
+      expect(screen.getByText(label)).toBeInTheDocument();
+      const radiogroup = screen.getByRole('radiogroup');
+      expect(radiogroup).toHaveAccessibleName(label);
+    });
+    it('should be selectable', async () => {
+      render(
+        <RadioButtonGroup>
+          <RadioButton value={1} />
+          <RadioButton value={2} />
+          <RadioButton value={3} />
+        </RadioButtonGroup>,
+      );
+      const radio = screen.getAllByRole('radio')[0];
+      expect(radio).not.toBeChecked();
+      await userEvent.click(radio);
+      expect(radio).toBeChecked();
+    });
     it('should set default checked uncontrolled state', () => {
       render(TestComponent);
       const radio = screen.getAllByRole('radio')[0];
@@ -56,19 +81,6 @@ describe('<RadioButton>', () => {
       await userEvent.click(radio);
 
       expect(radio).toHaveFocus();
-    });
-    it('should make children readOnly', async () => {
-      render(
-        <RadioButtonGroup readOnly>
-          <RadioButton />
-          <RadioButton />
-        </RadioButtonGroup>,
-      );
-      const radios = screen.getAllByRole('radio');
-
-      radios.forEach(radio => {
-        expect(radio).toHaveAttribute('aria-readonly', 'true');
-      });
     });
     it('should get focus on Tab', async () => {
       render(TestComponent);
@@ -83,7 +95,7 @@ describe('<RadioButton>', () => {
       await userEvent.click(radio);
       expect(radio).toBeChecked();
     });
-    it('should have radiogroup as role', () => {
+    it('should render radiogroup', () => {
       render(
         <RadioButtonGroup>
           <RadioButton />
@@ -115,7 +127,73 @@ describe('<RadioButton>', () => {
       );
       const radio = screen.getByRole('radio');
       expect(radio).toHaveAccessibleDescription(errorMessage);
-      expect(radio).toHaveAttribute('aria-invalid', 'true');
+      expect(radio).toBeInvalid();
+    });
+    it('should make all radio buttons required when inside a required radio button group', () => {
+      render(
+        <RadioButtonGroup required>
+          <RadioButton />
+          <RadioButton />
+        </RadioButtonGroup>,
+      );
+      const radios = screen.getAllByRole('radio');
+      expect(radios[0]).toBeRequired();
+      expect(radios[1]).toBeRequired();
+    });
+    it('should disable all radio buttons in a disabled radio button group', async () => {
+      render(
+        <RadioButtonGroup disabled>
+          <RadioButton />
+          <RadioButton />
+        </RadioButtonGroup>,
+      );
+
+      const radios = screen.getAllByRole('radio');
+
+      expect(radios[0]).toBeDisabled();
+      expect(radios[1]).toBeDisabled();
+
+      expect(radios[0]).not.toBeChecked();
+      await userEvent.click(radios[0]);
+      expect(radios[0]).not.toBeChecked();
+
+      expect(radios[1]).not.toBeChecked();
+      await userEvent.click(radios[1]);
+      expect(radios[1]).not.toBeChecked();
+    });
+
+    it('should make all radio buttons inside a read only radio button group read only', async () => {
+      render(
+        <RadioButtonGroup readOnly>
+          <RadioButton value={1} />
+          <RadioButton value={2} />
+        </RadioButtonGroup>,
+      );
+      const radios = screen.getAllByRole('radio');
+
+      radios.forEach(radio => {
+        expect(radio).toHaveAttribute('aria-readonly', 'true');
+      });
+
+      await userEvent.click(radios[0]);
+      expect(radios[0]).not.toBeChecked();
+    });
+
+    it('should select a radio with keyboard input', async () => {
+      render(
+        <RadioButtonGroup>
+          <RadioButton value={1} />
+          <RadioButton value={2} />
+          <RadioButton value={3} />
+        </RadioButtonGroup>,
+      );
+      const radio0 = screen.getAllByRole('radio')[0];
+      const radio1 = screen.getAllByRole('radio')[1];
+      await userEvent.keyboard('[Tab]');
+      await userEvent.keyboard('[ArrowRight]');
+
+      expect(radio1).toBeChecked();
+      expect(radio0).not.toBeChecked();
     });
   });
   describe('<RadioButtonGroup> controlled', () => {
