f37f8ce04e7047baec3424b67ec91fcf9beb8133
Bendik Solheim <bendik.solheim@bekk.no>
2025-08-12 14:08:06 +0200

    Sett alltid instanceId på ReactSelect
    Vi opplever hydration-utfordringer med Next.js på Select-komponenten.
    Problemet ser ut til å være at Emotion genererer ulike IDer på klient og server:
    ```
    +  id="react-select-3-live-region"
    -  id="react-select-2-live-region"
    ```
    Dette er et kjent problem, se github-issue for forklaring:
    https://github.com/JedWatson/react-select/issues/5459

--- packages/dds-components/src/components/Select/Select.tsx
+++ packages/dds-components/src/components/Select/Select.tsx
@@ -125,6 +125,7 @@ export function Select<Option = unknown, IsMulti extends boolean = false>({
   onKeyDown,
   openMenuOnClick,
   ref,
+  instanceId,
   ...rest
 }: SelectProps<Option, IsMulti>) {
   const themeContext = useContext(ThemeContext);
@@ -169,6 +170,7 @@ export function Select<Option = unknown, IsMulti extends boolean = false>({
         ? false
         : true,
     isMulti,
+    instanceId: instanceId ?? useId(),
     inputId: uniqueId,
     name: uniqueId,
     menuPortalTarget: portalTarget,
