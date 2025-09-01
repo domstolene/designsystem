11625abb6581aae4e1fd4c93608cf8af0e47465a
Agata Jedryszek <agata.jedryszek@domstol.no>
2025-09-01 13:00:40 +0200

    ELSA1-643 Støtte for `color` prop i `<InlineButton>`
    Støtter farger fra `@dds-design-tokens` + custom. Migrerer Storybook docs til `<Tabs>`-visning.

--- packages/dds-components/src/components/InlineButton/InlineButton.tsx
+++ packages/dds-components/src/components/InlineButton/InlineButton.tsx
@@ -4,17 +4,24 @@ import styles from './InlineButton.module.css';
 import { cn } from '../../utils';
 import { StylelessButton } from '../helpers';
 import focusStyles from '../helpers/styling/focus.module.css';
+import { type BaseTypographyProps, getColorCn } from '../Typography';
 import typographyStyles from '../Typography/typographyStyles.module.css';
 
-export type InlineButtonProps = ComponentPropsWithRef<'button'>;
+export type InlineButtonProps = Pick<BaseTypographyProps, 'color'> &
+  Omit<ComponentPropsWithRef<'button'>, 'color'>;
 
-export const InlineButton = ({ className, ...rest }: InlineButtonProps) => (
+export const InlineButton = ({
+  className,
+  color,
+  ...rest
+}: InlineButtonProps) => (
   <StylelessButton
     className={cn(
       className,
       focusStyles.focusable,
       typographyStyles.a,
       styles.button,
+      getColorCn(color),
     )}
     {...rest}
   />
