4f337cf479d4d56382f28647fbd41427e412f043
Agata Jedryszek <agata.jedryszek@domstol.no>
2026-01-20 12:52:19 +0100

    ELSA1-787: Fjerner enkelte ikoner
    Fjerner ikoner so ikke brukes.
    
    🚨 breaking:
    
    Enkelte ikoner brukes idag, men vi skal snart migrere til et bedre ikonsett. Annet eksisterende ikon skal brukes i de tilfellene. Se detaljer i [migreringsguiden v21 til v22](https://design.domstol.no/987b33f71/p/16deeb-v21-til-v22).

--- packages/dds-components/src/components/Icon/icons/chevronLargeRight.tsx
+++
@@ -1,8 +0,0 @@
-import { type SvgProps, SvgWrapper } from '../utils';
-export function ChevronLargeRightIcon(props: SvgProps) {
-  return (
-    <SvgWrapper {...props}>
-      <path d="M6.49 20.13l1.77 1.77 9.9-9.9-9.9-9.9-1.77 1.77L14.62 12l-8.13 8.13z" />
-    </SvgWrapper>
-  );
-}
--- packages/dds-components/src/components/Icon/icons/hourglassFull.tsx
+++
@@ -1,8 +0,0 @@
-import { type SvgProps, SvgWrapper } from '../utils';
-export function HourglassFullIcon(props: SvgProps) {
-  return (
-    <SvgWrapper {...props}>
-      <path d="M6 2v6h.01L6 8.01 10 12l-4 4 .01.01H6V22h12v-5.99h-.01L18 16l-4-4 4-3.99-.01-.01H18V2H6z" />
-    </SvgWrapper>
-  );
-}
