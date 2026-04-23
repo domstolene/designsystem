c31d643a850b5ed94f4bf2cdc5ff10ac40838c20
Agata Jedryszek <agata.jedryszek@domstol.no>
2026-04-23 12:42:21 +0200

    init

--- packages/dds-components/src/components/Accordion/Accordion.stories.tsx
+++ packages/dds-components/src/components/Accordion/Accordion.stories.tsx
@@ -28,6 +28,8 @@ const meta = preview.meta({
   decorators: [ddsProviderDecorator],
 });
 
+export default meta;
+
 export const Preview = meta.story({
   render: (args): JSX.Element => (
     <Accordion {...args}>
