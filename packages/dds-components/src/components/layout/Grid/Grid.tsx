7e677fae7b4b1577a199e85f24159bdb2a7b576f
Agata Jedryszek <agata.jedryszek@domstol.no>
2026-06-10 13:34:47 +0200

    chore: refaktorerer `<Grid>` default page layout

--- packages/dds-components/src/components/layout/Grid/Grid.tsx
+++ packages/dds-components/src/components/layout/Grid/Grid.tsx
@@ -10,6 +10,24 @@ import { Box } from '../Box';
 import { type ResponsiveGridProps } from '../common/Responsive.types';
 import { getResponsiveCSSProperties } from '../common/utils';
 
+const DEFAULT_PAGE_LAYOUT = {
+  gridTemplateColumns: {
+    xs: 'repeat(var(--dds-grid-xs-count), minmax(0, 1fr))',
+    sm: 'repeat(var(--dds-grid-sm-count), minmax(0, 1fr))',
+    md: 'repeat(var(--dds-grid-md-count), minmax(0, 1fr))',
+    lg: 'repeat(var(--dds-grid-lg-count), minmax(0, 1fr))',
+    xl: 'repeat(var(--dds-grid-xl-count), minmax(0, 1fr))',
+  },
+  gutterSize: {
+    xs: 'var(--dds-grid-xs-gutter-size)',
+    sm: 'var(--dds-grid-sm-gutter-size)',
+    md: 'var(--dds-grid-md-gutter-size)',
+    lg: 'var(--dds-grid-lg-gutter-size)',
+    xl: 'var(--dds-grid-xl-gutter-size)',
+  },
+  marginInline: { xs: 'x2', sm: 'x2', md: 'x4', lg: 'x6', xl: 'x10' },
+} as const;
+
 export type GridProps<T extends ElementType = 'div'> =
   PolymorphicBaseComponentProps<
     T,
@@ -25,6 +43,7 @@ export type GridProps<T extends ElementType = 'div'> =
         */
       gridTemplateColumns?: ResponsiveGridProps['gridTemplateColumns'];
       /** Om standard styling for sideoppsett skal brukes.
+       * Ved `true` settes overskrivbare standard verdier for `grid-template-columns`, `marginInline`, `rowGap` og `columnGap` som er tilpasset sideoppsett.
        * @default true
        */
       defaultPageLayout?: boolean;
@@ -51,49 +70,15 @@ export const Grid = <T extends ElementType = 'div'>({
   style,
   ...rest
 }: GridProps<T>) => {
-  const defaultLayoutStyles = {
-    gridTemplateColumns: gridTemplateColumns
-      ? gridTemplateColumns
-      : defaultPageLayout
-        ? {
-            xs: 'repeat(var(--dds-grid-xs-count), minmax(0, 1fr))',
-            sm: 'repeat(var(--dds-grid-sm-count), minmax(0, 1fr))',
-            md: 'repeat(var(--dds-grid-md-count), minmax(0, 1fr))',
-            lg: 'repeat(var(--dds-grid-lg-count), minmax(0, 1fr))',
-            xl: 'repeat(var(--dds-grid-xl-count), minmax(0, 1fr))',
-          }
-        : undefined,
-    columnGap: columnGap
-      ? columnGap
-      : defaultPageLayout
-        ? {
-            xs: 'var(--dds-grid-xs-gutter-size)',
-            sm: 'var(--dds-grid-sm-gutter-size)',
-            md: 'var(--dds-grid-md-gutter-size)',
-            lg: 'var(--dds-grid-lg-gutter-size)',
-            xl: 'var(--dds-grid-xl-gutter-size)',
-          }
-        : undefined,
-    rowGap: rowGap
-      ? rowGap
-      : defaultPageLayout
-        ? {
-            xs: 'var(--dds-grid-xs-gutter-size)',
-            sm: 'var(--dds-grid-sm-gutter-size)',
-            md: 'var(--dds-grid-md-gutter-size)',
-            lg: 'var(--dds-grid-lg-gutter-size)',
-            xl: 'var(--dds-grid-xl-gutter-size)',
-          }
-        : undefined,
-    marginInline: marginInline
-      ? marginInline
-      : defaultPageLayout
-        ? { xs: 'x2', sm: 'x2', md: 'x4', lg: 'x6', xl: 'x10' }
-        : undefined,
-  };
+  const pageDefaults = defaultPageLayout ? DEFAULT_PAGE_LAYOUT : null;
+  const resolvedGridTemplateColumns =
+    gridTemplateColumns ?? pageDefaults?.gridTemplateColumns;
+  const resolvedColumnGap = columnGap ?? pageDefaults?.gutterSize;
+  const resolvedRowGap = rowGap ?? pageDefaults?.gutterSize;
+  const resolvedMarginInline = marginInline ?? pageDefaults?.marginInline;
   const styleVariables = {
     ...getResponsiveCSSProperties(
-      defaultLayoutStyles.gridTemplateColumns,
+      resolvedGridTemplateColumns,
       'r',
       'grid-template-columns',
     ),
@@ -116,8 +101,7 @@ export const Grid = <T extends ElementType = 'div'>({
         id,
         cn(
           className,
-          defaultLayoutStyles.gridTemplateColumns &&
-            styles['dds-grid-template-columns'],
+          resolvedGridTemplateColumns && styles['dds-grid-template-columns'],
           grid && styles['dds-grid'],
           gridAutoColumns && styles['dds-grid-auto-columns'],
           gridAutoFlow && styles['dds-grid-auto-flow'],
@@ -131,9 +115,9 @@ export const Grid = <T extends ElementType = 'div'>({
         rest,
       )}
       display={display}
-      marginInline={defaultLayoutStyles.marginInline}
-      columnGap={defaultLayoutStyles.columnGap}
-      rowGap={defaultLayoutStyles.rowGap}
+      marginInline={resolvedMarginInline}
+      columnGap={resolvedColumnGap}
+      rowGap={resolvedRowGap}
     />
   );
 };
