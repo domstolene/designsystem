3bff8690fd2e4fa9e933fcf999ee6db8e17c0b62
Agata Jedryszek <agata.jedryszek@domstol.no>
2026-09-22 15:38:05 +0200

    fix(datepicker/timepicker): spacing og størrelser

---
+++ .changeset/chilly-geese-camp.md
@@ -0,0 +1,5 @@
+---
+'@norges-domstoler/dds-components': patch
+---
+
+Fikser bredden på inputsegmentene i `<DatePicker>` og `<TimePicker>`, slik at det ikke hopper når brukeren skriver verdien. Legger også padding på hvert segment, så de ser romsligere ut ved fokus. Reduserer samtidig spacing mellom saparator (. eller :) og segment. `<DatePicker>` og `<TimePicker>` har dermed litt mindre bredde i de fleste størrelsene.
--- packages/dds-components/src/components/date-inputs/DatePicker/DateField/DateSegment.tsx
+++ packages/dds-components/src/components/date-inputs/DatePicker/DateField/DateSegment.tsx
@@ -30,13 +30,13 @@ export function DateSegment({
 }: DateSegmentProps) {
   const ref = useRef<HTMLDivElement>(null);
   const { segmentProps } = useDateSegment(segment, state, ref);
+  const isSeparator = segment.type === 'literal';
   return (
     <Box
       {...segmentProps}
-      padding="0 0.05rem"
-      width="max-content"
-      height="max-content"
+      padding={isSeparator ? undefined : 'x0.125'}
       marginBlock="auto"
+      marginInline={isSeparator ? '-0.25ch' : undefined}
       aria-describedby={spaceSeparatedIdListGenerator([
         errorMessageId ? errorMessageId : undefined,
         tipId ? tipId : undefined,
@@ -49,10 +49,6 @@ export function DateSegment({
       )}
       style={{
         ...segmentProps.style,
-        minWidth:
-          segment.maxValue != null
-            ? String(segment.maxValue).length + 'ch'
-            : undefined,
       }}
     >
       <span
